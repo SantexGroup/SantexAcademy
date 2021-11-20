92179c30eaece4d73879091ab397b5be39ce7882
danielpalacio79 <111745@tecnicatura.frc.utn.edu.ar>
2021-11-20 16:18:36 -0300

    busqueda de raza segun id

--- back/controllers/breed.controller.js
+++ back/controllers/breed.controller.js
@@ -1,18 +1,27 @@
 const breedService = require('../services/breed.services');
 
-async function breedsList(req, res, next){
-  try{
-    //const { page } = req.params || 1;
+async function breedsList(req, res, next) {
+  try {
     const list = await breedService.getAll();
-    //console.log(list);
     res.json(list);
-
+  } catch (error) {
+    next(error);
   }
-  catch(error){
+}
+
+async function breedInfo(req, res, next) {
+  try {
+    const { id } = req.params;
+    const breedData = await breedService.getOne({
+      id,
+    });
+    res.json(breedData);
+  } catch (error) {
     next(error);
   }
 }
 
 module.exports = {
-  breedsList,  
+  breedsList,
+  breedInfo,
 };
--- back/services/breed.services.js
+++ back/services/breed.services.js
@@ -1,26 +1,33 @@
-const breedModel = require("../models").Razas;
-//const BreedExistException = require("../exceptions/breedExist.exception");
+const breedModel = require('../models').Razas;
+const BreedNotExistException = require('../exceptions/breedNotExist.exception');
 
 async function existeRaza(id) {
-  let busqueda = { id: id };
-  let tmp = await breedModel.findOne({
+  const busqueda = { id };
+  const tmp = await breedModel.findOne({
     where: busqueda,
+    attributes: ['raza'],
   });
-
   return tmp;
 }
 
-
 async function getAll() {
   const result = await breedModel.findAll({
-    attributes: ["id", "raza", "peligroso"],
+    attributes: ['id', 'raza', 'peligroso'],
   });
-
 
   return result;
 }
 
+async function getOne(data) {
+  const existe = await existeRaza(data.id);
+  if (existe) {
+    return existe;
+  }
+  throw new BreedNotExistException();
+}
+
 module.exports = {
-    existeRaza,
-    getAll,
+  existeRaza,
+  getAll,
+  getOne,
 };
