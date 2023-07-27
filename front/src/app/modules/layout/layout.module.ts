0629cf3cc75ee5402c07da1bb145e4455b1de24b
juanem <juanem@hotmail.com>
2023-07-27 19:11:06 -0300

    incorporando componentes de layout

--- front/src/app/modules/layout/layout.module.ts
+++ front/src/app/modules/layout/layout.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
