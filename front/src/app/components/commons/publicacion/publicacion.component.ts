1d9290ffde4987bb6aa066d2cf65023904787389
juanem <juanem@hotmail.com>
2023-07-29 14:49:04 -0300

    se crea pagina del noc para ver cursos y publicaciones, se crean componentes de curso y publicacion, se acomodan los ruteos del navegador

---
+++ front/src/app/components/commons/publicacion/publicacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publicacion',
+  templateUrl: './publicacion.component.html',
+  styleUrls: ['./publicacion.component.css']
+})
+export class PublicacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
