ab02f87f63a22112d916bceb7bbac58bcbadcc41
juanem <juanem@hotmail.com>
2023-07-31 19:02:30 -0300

    se crean paginas de precios y carrito, se crean componentes loginform y register form para mostrar en el home

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/pages/home/home.component';
 import { CursosComponent } from './modules/pages/cursos/cursos.component';
+import { PreciosComponent } from './modules/pages/precios/precios.component';
+import { CarritoComponent } from './modules/pages/carrito/carrito.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'cursos', component: CursosComponent}
+  { path: 'cursos', component: CursosComponent},
+  { path: 'precios', component: PreciosComponent},
+  { path: 'carrito', component: CarritoComponent}
   // ,
   // { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)}
   // },
