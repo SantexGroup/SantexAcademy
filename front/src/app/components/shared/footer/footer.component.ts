98beb9e5526ef53c3dae4f204c1f693d72661e50
ExequielMaydana <exedevcoding22@gmail.com>
2023-08-02 13:09:14 -0300

    add footer

--- front/src/app/components/shared/footer/footer.component.ts
+++ front/src/app/components/shared/footer/footer.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
