bbe8e6307dd76e60dfbfd8fe02a3c2e7cc6d76f2
Javier Lencina <carlosjavier.lencina@gmail.com>
2023-08-03 10:57:06 -0300

    Se inica con la creacion del modulo para el registro del usuario

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './modules/register/register-page/register-page.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
   },
-  {
+/*   {
     path: '**',
     redirectTo: 'dashboard'
+  },  */
+  {
+    path: 'registro',
+    component: RegisterPageComponent
   }
 ];
 
---
+++ front/src/app/modules/register/register.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterPageComponent } from './register-page/register-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    RegisterPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegisterModule { }
