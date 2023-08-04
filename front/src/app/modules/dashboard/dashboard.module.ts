48966da526eb294a8e607b4f1abbb6590a23e2c4
Nicolas <martinnicolascontrera@gmail.com>
2023-08-04 11:41:05 -0300

    Hice el componente footer y una parte de la sección principal en la página de inicio.

--- front/src/app/modules/dashboard/dashboard.module.ts
+++ front/src/app/modules/dashboard/dashboard.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { MaterialModule } from '../material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [DashboardPageComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    MaterialModule
+    MaterialModule,
+    SharedModule
   ],
   exports: [
     DashboardPageComponent
