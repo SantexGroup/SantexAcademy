504a4be8084915d7e62ccac160a8c64c61f49b1d
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-06 14:35:11 -0300

    Importación de CoreModule

--- front/src/app/modules/index/index.module.ts
+++ front/src/app/modules/index/index.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
+import { IndexPageComponent } from './index-page/index-page.component';
+import { RegistroVoluntariosComponent } from './registro-voluntarios/registro-voluntarios.component';
+import { RegistroOrganizacionComponent } from './registro-organizacion/registro-organizacion.component';
+import { LoginComponent } from './login/login.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexPageComponent,
+    RegistroVoluntariosComponent,
+    RegistroOrganizacionComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    IndexRoutingModule
+    IndexRoutingModule,
+    CoreModule
   ]
 })
 export class IndexModule { }
