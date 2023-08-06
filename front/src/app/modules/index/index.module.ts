6cfba0d7eb3baed6702ea3f2fb8739a11ceb2d91
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-06 13:31:38 -0300

    Creacion de indexModule y su routing

---
+++ front/src/app/modules/index/index.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IndexRoutingModule } from './index-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IndexRoutingModule
+  ]
+})
+export class IndexModule { }
