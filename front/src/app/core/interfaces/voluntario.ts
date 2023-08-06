75764abfc54b9d0beb70625a269656dcbdc96651
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-06 20:51:20 -0300

    Creacion de la interfaz Voluntario

---
+++ front/src/app/core/interfaces/voluntario.ts
@@ -0,0 +1,11 @@
+export interface Voluntario {
+    id_volunteer:number,
+    name:string,
+    lastname: string,
+    dni: number,
+    email: string,
+    password: string,
+    address: string,
+    points: number,
+    phone: string
+}
