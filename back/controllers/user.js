9dbe29c1d97106a38934551c1d3b8d7abc15f3ef
Lujan Navarra <lujinavarra@gmail.com>
2023-08-07 09:03:15 -0300

    ruta corregida

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,13 +1,12 @@
 // eslint-disable-next-line import/no-unresolved
 const services = require('../services/user');
 
-const { userService } = services;
-
+// const { userService } = services;
 const getUser = async (req, res) => {
   // eslint-disable-next-line prefer-destructuring
   const idUser = req.params.idUser;
   try {
-    const user = await userService.getUser(idUser);
+    const user = await services.getUser(idUser);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
