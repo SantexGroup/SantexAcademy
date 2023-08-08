1477a1dbd5605666f458b3aa72c512eb86bfb75b
MauriDemasi <mauridemasi88@gmail.com>
2023-08-08 18:30:08 -0300

    correccion modelado tablas

--- back/models/organizacion.js
+++ back/models/organizacion.js
@@ -4,77 +4,77 @@ module.exports = function (sequelize, DataTypes) {
       autoIncrement: true,
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
     },
     nombre: {
       type: DataTypes.STRING(100),
       allowNull: false,
-      unique: "nombre_UNIQUE"
+      unique: 'nombre_UNIQUE',
     },
     email: {
       type: DataTypes.STRING(50),
       allowNull: false,
-      unique: "email_UNIQUE"
+      unique: 'email_UNIQUE',
     },
     telefono: {
       type: DataTypes.STRING(25),
       allowNull: true,
-      unique: "telefono_UNIQUE"
+      unique: 'telefono_UNIQUE',
     },
     cuit: {
       type: DataTypes.STRING(11),
       allowNull: false,
-      unique: "cuit_UNIQUE"
+      unique: 'cuit_UNIQUE',
     },
     password: {
       type: DataTypes.STRING(45),
-      allowNull: false
-    }
+      allowNull: false,
+    },
   }, {
     sequelize,
     tableName: 'organizacion',
     timestamps: false,
     indexes: [
       {
-        name: "PRIMARY",
+        name: 'PRIMARY',
         unique: true,
-        using: "BTREE",
+        using: 'BTREE',
         fields: [
-          { name: "id" },
-        ]
+          { name: 'id' },
+        ],
       },
       {
-        name: "nombre_UNIQUE",
+        name: 'nombre_UNIQUE',
         unique: true,
-        using: "BTREE",
+        using: 'BTREE',
         fields: [
-          { name: "nombre" },
-        ]
+          { name: 'nombre' },
+        ],
       },
       {
-        name: "email_UNIQUE",
+        name: 'email_UNIQUE',
         unique: true,
-        using: "BTREE",
+        using: 'BTREE',
         fields: [
-          { name: "email" },
-        ]
+          { name: 'email' },
+        ],
       },
       {
-        name: "cuit_UNIQUE",
+        name: 'cuit_UNIQUE',
         unique: true,
-        using: "BTREE",
+        using: 'BTREE',
         fields: [
-          { name: "cuit" },
-        ]
+          { name: 'cuit' },
+        ],
       },
       {
-        name: "telefono_UNIQUE",
+        name: 'telefono_UNIQUE',
         unique: true,
-        using: "BTREE",
+        using: 'BTREE',
         fields: [
-          { name: "telefono" },
-        ]
+          { name: 'telefono' },
+        ],
       },
-    ]
+    ],
   });
 };
