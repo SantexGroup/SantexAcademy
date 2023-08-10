dcaac0b190c5429cc7e880f3a72b5c1688f399bd
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-10 00:53:54 -0300

    chore(cursos): Add Cursos Add component cursos folder into dashboard folder

---
+++ front/src/app/modules/dashboard/cursos/cursos.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CursosComponent } from './cursos.component';
+
+describe('CursosComponent', () => {
+  let component: CursosComponent;
+  let fixture: ComponentFixture<CursosComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CursosComponent]
+    });
+    fixture = TestBed.createComponent(CursosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front/src/app/modules/dashboard/cursos/cursos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent {
+
+}
