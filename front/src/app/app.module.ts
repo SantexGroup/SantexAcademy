859369c94f0677a8aacfb46f21208ed00581a756
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-09 23:52:38 -0300

    chore(app.module.ts): Modified app.module.ts Remove component file create accidentally

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
