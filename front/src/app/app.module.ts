1974eeca5528826bfcabcf192958fbd5f35c4a62
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-09 21:43:29 -0300

    chore(app.module.ts): Modified aap.module.ts Add import of components primeNG and primeFlex

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimengModule } from './lib/primeNG/primeng/primeng.module';
+import { PrimeflexModule } from './lib/primeFlex/primeflex/primeflex.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimengModule,
+    PrimeflexModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
