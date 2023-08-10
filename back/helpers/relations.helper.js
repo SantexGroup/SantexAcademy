58ffb1b8a1d6f17573e425d4cfdf36f1db1c39d3
PabloAndresBustos <pabloandresbustos@gmail.com>
2023-08-10 12:39:13 -0300

    Securizacion de las consultas a tablas intermedias

--- back/helpers/relations.helper.js
+++ back/helpers/relations.helper.js
@@ -8,20 +8,29 @@ async function addRelation(
   externalId,
   profileId,
 ) {
-  // Se requiere una muestra del objeto para poder saber el nombre de sus keys
-  const modelData = await model.findByPk(profileId);
-  // Se deescompone el objeto obteniendo el nombre de la Fk
-  const foreingKey = Object.keys(modelData.dataValues)[1];
-  // Se guardara todo en el objeto data
-  const data = {};
-  // Se asigna el valor de profileId brindado al ProfilesId del modelo
-  data.profilesId = profileId;
-  // Se asigna el valor de externalId brindado al Fk del modelo
-  data[foreingKey] = externalId;
-  // Se crea la realacion en la base de datos
-  const relation = await model.create(data);
+  // Se descompone el objeto obteniendo el nombre de la Fk
+  const foreingKey = Object.keys(model.rawAttributes)[1];
+  // Buscamos si la relacion ya existe
+  const relationExist = await model.findOne({
+    where: {
+      profilesId: profileId,
+      [foreingKey]: externalId,
+    },
+  });
 
-  return relation;
+  if (!relationExist) {
+    // Se guardara todo en el objeto data
+    const data = {};
+    // Se asigna el valor de profileId brindado al ProfilesId del modelo
+    data.profilesId = profileId;
+    // Se asigna el valor de externalId brindado al Fk del modelo
+    data[foreingKey] = externalId;
+    // Se crea la realacion en la base de datos
+    const relation = await model.create(data);
+
+    return relation;
+  }
+  throw new Error('La realacion ya existe');
 }
 /*
   Funcion que actualiza la realcion de Profile y otra tabla
@@ -30,25 +39,32 @@ async function addRelation(
   El id del profile
 */
 async function updateRelation(model, id, externalId, profileId) {
-  // Requerimos una muestra para conocer nombre de la Fk
-  const modelData = await model.findByPk(id);
   // Se deescompone el objeto obteniendo el nombre de la Fk
-  const externalKey = Object.keys(modelData.dataValues)[1];
-  // Se guardara todo en el objeto updateData
-  const updateData = {};
-  // Se asigna el valor de profileId brindado al ProfilesId del modelo
-  updateData.profilesId = profileId;
-  // Se asigna el valor de externalId brindado al Fk del modelo
-  updateData[externalKey] = externalId;
-  // Se procede con la actualizacion del modelo en la base de datos
-  const relationUpdate = await model.update({
-    ...updateData,
-  }, {
+  const foreingKey = Object.keys(model.rawAttributes)[1];
+  // Buscamos si la relacion existe
+  const relationExist = await model.findOne({
     where: {
       id,
     },
   });
-  return relationUpdate;
+  if (relationExist) {
+    // Se guardara todo en el objeto updateData
+    const updateData = {};
+    // Se asigna el valor de profileId brindado al ProfilesId del modelo
+    updateData.profilesId = profileId;
+    // Se asigna el valor de externalId brindado al Fk del modelo
+    updateData[foreingKey] = externalId;
+    // Se procede con la actualizacion del modelo en la base de datos
+    const relationUpdate = await model.update({
+      ...updateData,
+    }, {
+      where: {
+        id,
+      },
+    });
+    return relationUpdate;
+  }
+  throw new Error('La relacion no existe');
 }
 
 module.exports = {
