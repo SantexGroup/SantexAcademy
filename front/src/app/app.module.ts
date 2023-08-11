9ef61b38871b96548b74e3dbaa65c6b8f7ca23ef
Gonzalo Juarez <gi.juarezlinares@gmail.com>
2023-08-10 22:36:35 -0300

    terminando el body y empezando el footer

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyPageComponent } from './modules/body-page/body-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { FooterPageComponent } from './modules/footer-page/footer-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyPageComponent
+    BodyPageComponent,
+    FooterPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent,BodyPageComponent]
---
+++ front/src/app/modules/footer-page/footer-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-page',
+  templateUrl: './footer-page.component.html',
+  styleUrls: ['./footer-page.component.css']
+})
+export class FooterPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
