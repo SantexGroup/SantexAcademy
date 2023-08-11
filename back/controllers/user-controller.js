8bcafbde2b90bb18f76455bd4deb8c789776c351
erickechega17 <erickechegaray10@gmail.com>
2023-08-11 14:58:57 -0300

    funcionando endpoint /users/login

--- back/controllers/user-controller.js
+++ back/controllers/user-controller.js
@@ -1,11 +1,11 @@
 const userService = require('../services/user-service');
 
 async function login(req, res, next) {
-  console.log('funcionLogin');
-  const { name, password } = req.body;
+  //console.log('funcionLogin');
+  //const { name, password } = req.body;
 
   try {
-    const result = await userService.login(name, password);
+    const result = await userService.login();
     res.status(200).send(result);
   } catch (error) {
     next(error);
--- back/services/user-service.js
+++ back/services/user-service.js
@@ -1,34 +1,8 @@
-// const { NotAuthorizedException } = require('../exceptions/exceptions')
-
 const { User } = require('../models');
-// const jwt = require('jsonwebtoken')
-
-/* async function login(name, password) {
-  const user = await User.findOne({
-    where: {
-      name: name,
-      password: password
-    }
-  })
-
-  if (!user) {
-    throw new NotAuthorizedException("Email y/o clave incorrectos")
-  }
-
-  const token = jwt.sign({
-    id: user.id,
-    email: user.email,
-    name: user.name
-  }, 'admin')
-
-  return {
-    accessToken: token
-  }
-} */
 
 async function login() {
   const usuarios = await User.findAll();
-  console.log('USERS', usuarios);
+  //console.log('USERS', usuarios);
 
   return usuarios;
 }
