e2e921974c80a3179889d1d71a8532dc92860e92
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-11 11:51:46 -0300

    feat(courses): Create Courses Detail Create courses detail component file intro courses folder

---
+++ front/src/app/modules/dashboard/courses/courses-detail/courses-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-detail',
+  templateUrl: './courses-detail.component.html',
+  styleUrls: ['./courses-detail.component.scss']
+})
+export class CoursesDetailComponent {
+
+}
