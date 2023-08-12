6922f538c5deaee05c72329d377921feeb521406
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-12 20:18:13 -0300

    Add(dashboard): Route Componente Courses Add route into ashboard-routing.module.ts file

--- front/src/app/modules/dashboard/dashboard-routing.module.ts
+++ front/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CursosComponent } from './cursos/cursos.component';
+import { CoursesComponent } from './courses/courses.component';
+
+
 
 const routes: Routes = [
   {
     path: '',
-    component: CursosComponent,
+    component: CoursesComponent,
     children: [
       
     ]
