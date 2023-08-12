da9faea64502ae8933e2495dacb4e28acfbdfcd5
Gonzalo Juarez <gi.juarezlinares@gmail.com>
2023-08-12 15:42:22 -0300

    corrigiendo una coma que daba error en app module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UsuarioModule } from './modules/usuario/usuario.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,11 +28,12 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     MatExpansionModule,
     MatInputModule,
-    MatButtonModule,,
+    MatButtonModule,
     UsuarioModule,
     FormsModule,
     ReactiveFormsModule, 
     HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent,BodyPageComponent]
