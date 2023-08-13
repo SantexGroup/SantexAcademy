e9a88f8ae853859b608540e7e6817297fb03a98a
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-13 13:38:26 -0300

    Desarrollo de la lógica de voluntarioGuard

--- front/src/app/core/guards/voluntario.guard.ts
+++ front/src/app/core/guards/voluntario.guard.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { VoluntarioService } from '../services/voluntario.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoluntarioGuard implements CanLoad {
+  constructor(private voluntarioService:VoluntarioService, private router:Router){
+
+  }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      
+      //Controlamos que si el valor del observable credenciales usuario es nulo, no lo deje pasar y lo redirija al index, de lo contrario lo deja pasar
+      if(this.voluntarioService.credencialesVoluntario.value == null){
+        
+        this.router.navigate(['/index']);
+        return false;
+      }
+    
+    
+      return true;
   }
 }
