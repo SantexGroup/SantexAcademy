8d09680e4355c62594d06862a758a3741d27aa3e
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-13 13:41:12 -0300

    Configuración de ruteo para el componente voluntario

--- front/src/app/modules/voluntarios/voluntarios-routing.module.ts
+++ front/src/app/modules/voluntarios/voluntarios-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VoluntariosComponent } from './voluntarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:VoluntariosComponent
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
