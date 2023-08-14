543470eeffb864c12ab69c710ef1d4e6c9b5990a
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-14 16:32:45 -0300

    Creación de componente organizaciones y su configuración de ruteo

--- front/src/app/modules/organizaciones/organizaciones-routing.module.ts
+++ front/src/app/modules/organizaciones/organizaciones-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrganizacionesComponent } from './organizaciones.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:OrganizacionesComponent},
+  {path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ front/src/app/modules/organizaciones/organizaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organizaciones',
+  templateUrl: './organizaciones.component.html',
+  styleUrls: ['./organizaciones.component.css']
+})
+export class OrganizacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
