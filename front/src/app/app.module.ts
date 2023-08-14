2b9197f029cd514e06dada132024cd66b15760ba
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-14 17:24:44 -0300

    feat(app.module): Animations Add functionality as complement to the accordion component o the primeng library 	#	modificados:     front/src/app/app.module.ts

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule }from '@angular/platform-browser/animations'
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
