41bbea28be32e34a371c79b950ebc9b6aaff0e03
cgavernet <carlosgavernet@gmail.com>
2023-08-15 09:07:19 -0300

    feat(api): Add courseDetail model & migration file

---
+++ back/migrations/20230815120514-create-course-detail.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('courseDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      paragraph1: {
+        type: Sequelize.STRING
+      },
+      paragraph2: {
+        type: Sequelize.STRING
+      },
+      image1url: {
+        type: Sequelize.STRING
+      },
+      image2ur2: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('courseDetails');
+  }
+};
