950c6c91f74e3824cda7ce69892627622add69e8
Exequiel <eportero421@gmail.com>
2023-08-15 01:40:39 -0300

    feat: Add CarouselModule to primeng.module.ts Added the import statement for CarouselModule in primeng.module.ts to incorporate the functionality of the new component.

--- front/src/app/lib/primeNG/primeng/primeng.module.ts
+++ front/src/app/lib/primeNG/primeng/primeng.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { AccordionModule } from 'primeng/accordion';
+import {CarouselModule} from 'primeng/carousel'
 
 @NgModule({
   declarations: [],
@@ -15,6 +16,7 @@ import { AccordionModule } from 'primeng/accordion';
     ButtonModule,
     DividerModule,
     AccordionModule,
+    CarouselModule
   ]
 })
 export class PrimengModule { }
