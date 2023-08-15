b218422c2577d21d23f2b1c90953c527f96a3f89
cgavernet <carlosgavernet@gmail.com>
2023-08-15 16:10:50 -0300

    chore(api): Insert <br> into courseDetail

--- back/routes/courseDetail.js
+++ back/routes/courseDetail.js
@@ -5,6 +5,8 @@ const { courseDetailController } = require('../controllers');
 
 // Obtener todos los cursos
 router.get('/', courseDetailController.getAllCoursesDetails);
+
+// Crear un nuevo curso
 router.post('/', courseDetailController.createCourseDetail);
 
 module.exports = router;
