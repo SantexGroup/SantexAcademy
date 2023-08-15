a1475fa4a1fc1ce5f93771da38b3929c3d0531e7
Gerardo Fabian Quispe <gfq0684@gmail.com>
2023-08-15 19:29:29 -0300

    agregamos los vinculos del navbar

---
+++ front/src/app/modules/dashboard/nav-bar/nav-bar-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormUpdateComponent } from '../form-update/form-update.component';
+import { NavBarModule } from './nav-bar.module';
+import { FormAddProductComponent } from '../form-add-product/form-add-product.component';
+
+const routes: Routes = [
+  {
+    path: 'add-product',
+    component: FormAddProductComponent
+  },
+  {
+    path: 'form-update',
+    component: FormUpdateComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class NavbarRoutingModule { }
