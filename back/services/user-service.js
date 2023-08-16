05c1ca7933e35bdac8b900a2f6f62650de386c82
Leonardo Cortes <leosirx@gmail.com>
2023-08-16 12:45:36 -0300

    se creo el endpoint de regsitro de usuario

--- back/services/user-service.js
+++ back/services/user-service.js
@@ -1,18 +1,36 @@
 const { User } = require('../models');
 
 async function login(alias, password) {
-  const users = await User.findOne({ 
-    where: { 
-        alias: alias,
-        password: password
-    } 
-});
+  const users = await User.findOne({
+    where: {
+      alias,
+      password,
+    },
+  });
 
-if (!users) {
-    throw new Error("Alias o Contraseña incorrectos");
-}
+  if (!users) {
+    throw new Error('Alias o Contraseña incorrectos');
+  }
 
   return users;
 }
 
-module.exports = { login };
+// creacion de usuario
+async function userRegister(idDireccion, firstName, lastName, dni, mail, password,
+  estadoDeVendedor, alias) {
+  const user = new User();
+
+  user.idDireccion = idDireccion;
+  user.firstName = firstName;
+  user.lastName = lastName;
+  user.dni = dni;
+  user.mail = mail;
+  user.password = password;
+  user.estadoDeVendedor = estadoDeVendedor;
+  user.alias = alias;
+
+  const userCreated = await user.save();
+  return userCreated;
+}
+
+module.exports = { login, userRegister };
