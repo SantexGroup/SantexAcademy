015af975e470ca99820bee881815aa15ae5fb30f
ismael <ismaelreta.programacion@gmail.com>
2023-08-16 14:52:52 -0300

    removed comment

--- back/services/userService.js
+++ back/services/userService.js
@@ -4,15 +4,4 @@ const getUser = async (id) => userProvider.getUser(id);
 
 const createUser = async (user) => userProvider.createUser(user);
 
-/*
-const getUser = async (id) => {
-  return await userProvider.getUser(id);
-};
-
-const createUser = async (user) => {
-  return await userProvider.createUser(user);
-};
-
-*/
-
 module.exports = { getUser, createUser };
