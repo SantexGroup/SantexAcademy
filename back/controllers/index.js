93d43257759e4e6d0a264765e8dceacbfd424a41
jonimarqu <marquez.jonas91@gmail.com>
2023-08-17 20:09:03 -0300

    Add product controller.

--- back/controllers/index.js
+++ back/controllers/index.js
@@ -1,4 +1,5 @@
-const catalogoController = require('./catalogo');
-const orgController = require('./organizaciones');
+const catalogoController = require("./catalogo");
+const orgController = require("./organizaciones");
+const productController = require("./producto");
 
-module.exports = { catalogoController, orgController };
+module.exports = { catalogoController, orgController, productController };
