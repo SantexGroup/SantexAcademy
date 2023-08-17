fe4068ce675577df1f9c0e84efa84077d877f66a
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-17 00:19:20 -0300

    feat(courses): Create Component Add component pay-transf-course files into components folder

---
+++ front/src/app/components/courses/pay-transf-course/pay-transf-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-transf-course',
+  templateUrl: './pay-transf-course.component.html',
+  styleUrls: ['./pay-transf-course.component.scss']
+})
+export class PayTransfCourseComponent {
+
+}
