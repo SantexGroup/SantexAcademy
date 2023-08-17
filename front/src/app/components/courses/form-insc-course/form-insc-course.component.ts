4f9a0608d5cb0c375a77d32faf50f5a1c6f884b0
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-17 00:16:37 -0300

    feat(courses): Create Component Add form-insc-course files into components folder

---
+++ front/src/app/components/courses/form-insc-course/form-insc-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-insc-course',
+  templateUrl: './form-insc-course.component.html',
+  styleUrls: ['./form-insc-course.component.scss']
+})
+export class FormInscCourseComponent {
+
+}
