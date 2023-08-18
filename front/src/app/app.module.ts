368910753fc7e2aa4bc0e561a0668d43c2b482ae
ayelen22 <ayelen220595@gmail.com>
2023-08-18 02:54:20 -0300

    Agregado de FormDelete

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/modules/dashboard/form-delete/form-delete.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-form-delete',
+  templateUrl: './form-delete.component.html',
+  styleUrls: ['./form-delete.component.css']
+})
+export class FormDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  titularAlerta: string ='';
+  showDelete() {
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+    return false;
+  }
+  mostrar() {
+    swal.fire('Registro exitoso .... ', this.titularAlerta);
+    return false;
+  }
+}
