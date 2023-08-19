9c4a1ef27a8ee29e8196d5e045f0e2a5397ccdf5
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-19 19:54:45 -0300

    Se agregaron los middlewares isAuthenticated para voluntarios y para organizacion

--- back/middleware/authentication.js
+++ back/middleware/authentication.js
@@ -1,15 +1,28 @@
 const passport = require('passport');
 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticatedVoluntario = (req, res, next) => {
+  // eslint-disable-next-line consistent-return
   passport.authenticate('jwt', { session: false }, (err, user, info) => {
-    if (err || !user) {
+    if (err || !user || info.tipo !== 'voluntario') {
       const error = ('Usuario no autorizado');
-
       return next(error);
     }
-
+    req.usuario = user;
     next();
   })(req, res, next);
 };
 
-module.exports = { isAuthenticated };
+const isAuthenticatedOrganizacion = (req, res, next) => {
+  // eslint-disable-next-line consistent-return
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err || !user || info.tipo !== 'organizacion') {
+      const error = ('Usuario no autorizado');
+
+      return next(error);
+    }
+    req.usuario = user;
+    next();
+  })(req, res, next);
+};
+
+module.exports = { isAuthenticatedVoluntario, isAuthenticatedOrganizacion };
