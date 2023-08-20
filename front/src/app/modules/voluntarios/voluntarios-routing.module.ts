b3a5c59835f8282b99d996370ed9d5254e44c2b4
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-20 12:11:57 -0300

    Se creó el componente tareas-voluntario y se configuró su ruteo

--- front/src/app/modules/voluntarios/voluntarios-routing.module.ts
+++ front/src/app/modules/voluntarios/voluntarios-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { VoluntariosComponent } from './voluntarios.component';
 import { DashboardVoluntarioComponent } from './dashboard-voluntario/dashboard-voluntario.component';
 import { PremiosVoluntarioComponent } from './premios-voluntario/premios-voluntario.component';
+import { TareasVoluntarioComponent } from './tareas-voluntario/tareas-voluntario.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path:'premios',
         component:PremiosVoluntarioComponent
+      },
+      {
+        path:'tareas',
+        component:TareasVoluntarioComponent
       }
     ]
   },
