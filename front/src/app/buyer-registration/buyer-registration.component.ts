a743172d75a8f513484c1c8112032853a9d41b88
pablo-maroto <pabloemaroto@gmail.com>
2023-08-20 17:19:05 -0300

    HU4-Agregado pantalla de registro de usuario

--- front/src/app/buyer-registration/buyer-registration.component.ts
+++ front/src/app/buyer-registration/buyer-registration.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-buyer-registration',
   templateUrl: './buyer-registration.component.html',
   styleUrls: ['./buyer-registration.component.css']
 })
+
+
 export class BuyerRegistrationComponent {
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
+  emailIsValid: boolean = true;
+
+  
+  validateEmail() {
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    this.emailIsValid = emailPattern.test(this.email);
+  }
 
   registerUser() {
+    if (!this.emailIsValid) {
+      alert('El correo electrónico no es válido');
+      return;
+    }
     if (this.password !== this.confirmPassword) {
       alert('Las contraseñas no coinciden');
       return;
     }
-    
-    // Lógica para hacer la llamada API para el registro del usuario
   }
 }
+
+
+
