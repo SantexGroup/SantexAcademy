59db1cf90c01f9da23b54d1a2eccfe4e75f6a05f
Exequiel <eportero421@gmail.com>
2023-08-19 22:33:01 -0300

    chore(service): Update CourseService to fetch data Update the existing CourseService to replace the JSON data with actual HTTP requests to the backend endpoint
    The 'getCourses()' method utilizes the HttpClient
    'get' method to fetch the list of courses from sethe server

--- front/src/app/services/courses.service.ts
+++ front/src/app/services/courses.service.ts
@@ -1,59 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../core/interface/courses.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  constructor() {}
-  
-  courses: Course[] = [
-    {
-      id: 1,
-      title: 'Sublimación a máquina',
-      description:
-        'Curabitur in libero ut massa volutpat convallis morbi odio odio odio odio odio',
-      price: 2000,
-      duration: '13/08/2022',
-      starter_date: '20/09/2022',
-    },
-    {
-      id: 2,
-      title: 'Sublimación a máquina',
-      description:
-        'Curabitur in libero ut massa volutpat convallis morbi odio odio odio odio odio',
-      price: 2000,
-      duration: '24/12/2022',
-      starter_date: '20/01/2023',
-    },
-    {
-      id: 3,
-      title: 'Sublimación a máquina',
-      description:
-        'Curabitur in libero ut massa volutpat convallis morbi odio odio odio odio odio',
-      price: 2000,
-      duration: '17/10/2022',
-      starter_date: '13/05/2023',
-    },
-    {
-      id: 4,
-      title: 'Sublimación a máquina',
-      description:
-        'Curabitur in libero ut massa volutpat convallis morbi odio odio odio odio odio',
-      price: 2000,
-      duration: '23/03/2023',
-      starter_date: '24/08/2022',
-    },
-    {
-      id: 5,
-      title: 'Sublimación a máquina',
-      description:
-        'Curabitur in libero ut massa volutpat convallis morbi odio odio odio odio odio',
-      price: 2000,
-      duration: '12/09/2022',
-      starter_date: '29/09/2022',
-    },
-  ];
+
+  private endpoint = 'http://localhost:4001/courses' //creamos una variable para el endpoint de los cursos
+
+  constructor(private http: HttpClient) {} //creamos una variable para el modulo de HttpClient
+
+  getPosts():Observable<Course[]> { //creamos un metodo que va a devolver un observable con el array de cursos
+
+    return this.http.get<Course[]>(this.endpoint) //hacemos un get del endpoint que será el array de cursos
+
+  }
+
 
   responsiveOptions: any[] | undefined =  [
     {
