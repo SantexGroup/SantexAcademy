025505c7195f882493f72f4c804dddb137e8c30b
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-20 11:48:30 -0300

    Se creó la barra de navegacion en componente voluntarios

--- front/src/app/modules/voluntarios/voluntarios.component.ts
+++ front/src/app/modules/voluntarios/voluntarios.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { VoluntarioService } from 'src/app/core/services/voluntario.service';
 
@@ -8,10 +9,17 @@ import { VoluntarioService } from 'src/app/core/services/voluntario.service';
   styleUrls: ['./voluntarios.component.css']
 })
 export class VoluntariosComponent implements OnInit {
+  @ViewChild('sideNav') sideNav!:MatSidenav;
+
 
   constructor(private voluntarioService:VoluntarioService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  redireccionarA(ruta:string):void{
+    this.router.navigate([ruta]);
+    this.sideNav.close();
   }
 
   cerrarSesion(){
