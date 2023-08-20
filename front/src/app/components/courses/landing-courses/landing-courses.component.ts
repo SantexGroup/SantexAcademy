380b24dbe1d4dacc8d067d1319d2bcdfb2bbeb88
Exequiel <eportero421@gmail.com>
2023-08-19 22:43:57 -0300

    feat(landing): Update landing-courses.ts Modify component to fetch backend course data using CourseService
    ngOnInit subscribes to getCourses() method, handles responses and errors

--- front/src/app/components/courses/landing-courses/landing-courses.component.ts
+++ front/src/app/components/courses/landing-courses/landing-courses.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CoursesService } from 'src/app/services/courses.service';
+import { Course } from 'src/app/core/interface/courses.interface';
 
 
 @Component({
@@ -7,8 +8,28 @@ import { CoursesService } from 'src/app/services/courses.service';
   templateUrl: './landing-courses.component.html',
   styleUrls: ['./landing-courses.scss'],
 })
-export class LandingCoursesComponent {
+export class LandingCoursesComponent implements OnInit {
+  courses: Course[] = [];
 
   constructor(public coursesService: CoursesService) {}
+
+  ngOnInit() { //cuando se cargue el componente va a recibir la información
+    this.coursesService.getPosts().subscribe(
+      (response) => {
+        this.courses = response; //si todo sale bien me va a dar la respuesta con los cursos
+        console.log('Esta es la respuesta' ,response)
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+
+  getRoundedPrice(price: number, due: number): number { //este metodo me redondea el valor de la cuota
+
+    return Math.round(price / due); //me redondea el precio que se divide por la cuota
+
+  }
 
 }
