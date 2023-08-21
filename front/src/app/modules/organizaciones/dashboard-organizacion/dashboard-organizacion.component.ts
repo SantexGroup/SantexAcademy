8e1d98c8099ee2bbf571109c86ce8761116f5243
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-20 22:28:41 -0300

    Se creó el componente dashboard-organización y se configuró su ruteo

---
+++ front/src/app/modules/organizaciones/dashboard-organizacion/dashboard-organizacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-organizacion',
+  templateUrl: './dashboard-organizacion.component.html',
+  styleUrls: ['./dashboard-organizacion.component.css']
+})
+export class DashboardOrganizacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- front/src/app/modules/organizaciones/organizaciones-routing.module.ts
+++ front/src/app/modules/organizaciones/organizaciones-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrganizacionesComponent } from './organizaciones.component';
+import { DashboardOrganizacionComponent } from './dashboard-organizacion/dashboard-organizacion.component';
 
 const routes: Routes = [
-  {path:'', component:OrganizacionesComponent},
+  {
+    path:'', 
+  component:OrganizacionesComponent,
+  children:[
+    {
+      path:'dashboard',
+      component:DashboardOrganizacionComponent
+    }
+  ]
+  },
   {path:'**', redirectTo:''}
 ];
 
