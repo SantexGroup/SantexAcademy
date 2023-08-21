e74aef3ffa69d24b53b43e78b98c5a1d63783740
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-21 17:19:57 -0300

    fix(app): Correct File Create failed file

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -7,18 +7,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule }from '@angular/platform-browser/animations'
 import { ComponentsModule } from './components/components.module';
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
+    
   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ComponentsModule
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
