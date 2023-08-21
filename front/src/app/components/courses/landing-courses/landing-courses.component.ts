c78803e818584483cfce81ee4daba9c95ac9bc27
Exequiel <eportero421@gmail.com>
2023-08-20 22:20:35 -0300

    feat(courses): Use the courses variable
    Utilize the variable 'courses' within the service to implement the
    course listing feature.

--- front/src/app/components/courses/landing-courses/landing-courses.component.ts
+++ front/src/app/components/courses/landing-courses/landing-courses.component.ts
@@ -9,14 +9,13 @@ import { Course } from 'src/app/core/interface/courses.interface';
   styleUrls: ['./landing-courses.scss'],
 })
 export class LandingCoursesComponent implements OnInit {
-  courses: Course[] = [];
 
   constructor(public coursesService: CoursesService) {}
 
   ngOnInit() { //cuando se cargue el componente va a recibir la información
     this.coursesService.getPosts().subscribe(
       (response) => {
-        this.courses = response; //si todo sale bien me va a dar la respuesta con los cursos
+        this.coursesService.courses = response; //si todo sale bien me va a dar la respuesta con los cursos
         console.log('Esta es la respuesta' ,response)
       },
       (error) => {
@@ -25,11 +24,5 @@ export class LandingCoursesComponent implements OnInit {
     );
   }
 
-
-  getRoundedPrice(price: number, due: number): number { //este metodo me redondea el valor de la cuota
-
-    return Math.round(price / due); //me redondea el precio que se divide por la cuota
-
-  }
 
 }
