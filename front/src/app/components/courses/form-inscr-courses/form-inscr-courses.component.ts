0bcca7ccac8dbcc92fd7056e7e21980dc66f8938
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-21 17:27:18 -0300

    feat(courses): Create component for Record Add components form-inscr-courses file for course record

---
+++ front/src/app/components/courses/form-inscr-courses/form-inscr-courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-inscr-courses',
+  templateUrl: './form-inscr-courses.component.html',
+  styleUrls: ['./form-inscr-courses.component.scss']
+})
+export class FormInscrCoursesComponent {
+
+}
