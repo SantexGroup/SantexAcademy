0347539b2166b8f63b748c473e177a771ec58c94
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-20 23:07:00 -0300

    Modificación de jwt interceptor para enviar token de organización

--- front/src/app/core/interceptors/jwt.interceptor.ts
+++ front/src/app/core/interceptors/jwt.interceptor.ts
@@ -9,27 +9,44 @@ import { Observable, take } from 'rxjs';
 import { VoluntarioService } from '../services/voluntario.service';
 import { Credencial } from '../interfaces/credencial';
 import { ApiService } from '../http/api.service';
+import { OrganizacionService } from '../services/organizacion.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor(private voluntarioService:VoluntarioService) {}
+  constructor(private voluntarioService:VoluntarioService, private organizacionService:OrganizacionService) {}
   credencialesVoluntario!:Credencial|null;
+  credencialesOrganizacion!:Credencial|null;
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     
     this.voluntarioService.getCredencialesVoluntario.pipe(take(1)).subscribe({
       next:(res)=>this.credencialesVoluntario = res
     });
     
+    this.organizacionService.getCredencialesOrganizacion.pipe(take(1)).subscribe({
+      next:(res)=> this.credencialesOrganizacion = res
+    });
+
     if(this.credencialesVoluntario!= null){
       request = request.clone({
         setHeaders:{
           Authorization:`Bearer ${this.credencialesVoluntario.token}`
         }
       });
-
+      return next.handle(request);  
+    }
+    else if(this.credencialesOrganizacion!= null){
+      request = request.clone({
+        setHeaders:{
+          Authorization:`Bearer ${this.credencialesOrganizacion.token}`
+        }
+      });
+      return next.handle(request);
+    }
+    else{
+      
+      return next.handle(request);
     }
     
-    return next.handle(request);
   }
 }
