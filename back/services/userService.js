bb8633be79be2b9bb56a32d151abe2da9e677b6d
ismael <ismaelreta.programacion@gmail.com>
2023-08-22 10:51:57 -0300

    remove unused code

--- back/services/userService.js
+++ back/services/userService.js
@@ -1,7 +1,5 @@
 const userProvider = require('../providers/userProvider');
 
-const getUser = async (id) => userProvider.getUser(id);
-
 const createUser = async (user) => userProvider.createUser(user);
 
-module.exports = { getUser, createUser };
+module.exports = { createUser };
