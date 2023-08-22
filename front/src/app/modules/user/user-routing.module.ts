ea05bdf384509ecaacdd2fa95c55f910cec006a4
FededelRincon <delrincon@gmail.com>
2023-08-22 17:19:06 -0300

    add userModule

---
+++ front/src/app/modules/user/user-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { UserComponent } from './components/user/user.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UserComponent,
+    children: []
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
--- front/src/app/modules/user/user.module.ts
+++ front/src/app/modules/user/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserRoutingModule } from './user-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule
   ]
 })
 export class UserModule { }
