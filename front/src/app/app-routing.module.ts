a490d9a51e83b2e8009eec63f2d23b3f9312db94
Sebastian <89744289+SebatianLedesma@users.noreply.github.com>
2023-08-22 22:50:05 -0300

    Update app-routing.module.ts
    Se descomenta path: 'register'

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,13 +6,12 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
-  // { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule) },
+  { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule) },
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home' }
 ];
 
-// el register no encuentro donde solucionarlo, o lo arregla otro, o directamente lo borro
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
