e7e147a166dac117084a82271b7e2fb7b0904ef4
PabloAndresBustos <pabloandresbustos@gmail.com>
2023-08-23 20:50:39 -0300

    union de los componentes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyPageComponent } from './modules/body-page/body-page.component';
 import { RegistroComponent } from './modules/usuario/registro/registro.component';
 import { LoginComponent } from './modules/usuario/login/login.component';
+import { HeaderPageComponent } from './modules/header-page/header-page.component';
+import { FooterPageComponent } from './modules/footer-page/footer-page.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
   },
-/*   {
-    path: '**',
-    redirectTo: 'dashboard'
-  },  */
+  {
+    path: '',
+    component: BodyPageComponent
+  }, 
   {
     path: 'registro',
     component: RegistroComponent
