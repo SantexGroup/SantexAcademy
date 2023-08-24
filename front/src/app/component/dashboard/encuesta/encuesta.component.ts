135f9428b3bb669e1cf79a720435e7fa5fcca490
Sol <stoledo7@hotmail.com>
2023-08-24 09:57:09 -0300

    conflicto resuelto

--- front/src/app/component/dashboard/encuesta/encuesta.component.ts
+++ front/src/app/component/dashboard/encuesta/encuesta.component.ts
@@ -1,24 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-
-=======
->>>>>>> a30f5a5e03c28ed0948424ba7494ce3ddc7b1f25
 @Component({
   selector: 'app-encuesta',
   templateUrl: './encuesta.component.html',
   styleUrls: ['./encuesta.component.css']
 })
 export class EncuestaComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> a30f5a5e03c28ed0948424ba7494ce3ddc7b1f25
