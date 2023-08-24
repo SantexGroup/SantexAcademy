1a53221c6cf050db3ebf24be7960c2cf5609d362
vani <you@example.com>
2023-08-24 15:47:52 -0300

    cree: auth/services

--- front/src/app/core/services/auth.service.ts
+++ front/src/app/core/services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class authService {
+  private apiUrl = 'url_de_tu_backend'; // Agrega la URL de tu backend aquí
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email:any ): Observable<any> {
+    const loginData = { email};
+    return this.http.post(`${this.apiUrl}/login`, loginData);
+  }
+
+  
 }
