5a786ee8d21b6cfd21c8c823604a9bb9f6316160
diegomoyano87 <diegomoyano_19@hotmail.com>
2023-08-24 19:44:01 -0300

    Se realiza rutas habiendo creado 2 componentes de prueba: PRUEBA y PAGE-NO-FOUND

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { PruebaComponent } from './component/prueba/prueba.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard'
-  }
+const routes: Routes = [ //defino las rutas
+
+{ path: 'prueba', component: PruebaComponent },
+{ path: '**', component: PageNotFoundComponent }, 
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes, {useHash: true})],
+   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+
+export class AppRoutingModule { } 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { PruebaComponent } from './component/prueba/prueba.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+//Rutas
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    NavbarComponent,  
+    PruebaComponent, BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
