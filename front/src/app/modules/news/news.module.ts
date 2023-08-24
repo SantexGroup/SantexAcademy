cb07e0975713785db123bb3e406c7551ae4d8e90
FededelRincon <delrincon@gmail.com>
2023-08-24 16:16:45 -0300

    delete homeModule, create landingModule y newsModule + update app-routing.module

---
+++ front/src/app/modules/news/news.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewsRoutingModule } from './news-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NewsRoutingModule
+  ]
+})
+export class NewsModule { }
