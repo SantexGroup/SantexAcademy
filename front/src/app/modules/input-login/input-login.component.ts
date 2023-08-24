95cb1bf76fa40e9502965a9658e7f90e09fd5148
vani <you@example.com>
2023-08-24 16:01:48 -0300

    modificacion:valid te email y logicas de manejos

--- front/src/app/modules/input-login/input-login.component.ts
+++ front/src/app/modules/input-login/input-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { authService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-input-login',
@@ -7,13 +9,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./input-login.component.css']
 })
 export class InputLoginComponent implements OnInit {
+  email = new FormControl("",[Validators.email,Validators.required])
+  
 
-  constructor(private router: Router) { }
 
+
+
+  constructor(private authservice: authService,  private router: Router) { }
   ngOnInit(): void {
-  }
-  redirectToContra(): void {
-    this.router.navigate(['']); //conponente passwd.
+  
   }
 
+  redirectToContra() {
+    if(this.email.valid==false){
+      console.log("error");
+      return;
+
+    }
+   
+    this.authservice.login(this.email.value).subscribe(
+
+      (response) => {
+        this.router.navigate(['/contra']); // Lógica de manejo de respuesta exitosa (por ejemplo, guardar token)
+        
+      },
+      (error) => {
+        console.error('Error en el inicio de sesión', error);
+        this.router.navigate(['/contra']);
+        // Lógica de manejo de error
+      }
+    );
+  }
 }
+
