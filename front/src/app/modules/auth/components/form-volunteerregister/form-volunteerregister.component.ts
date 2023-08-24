1a57b368096cc027a6dbfb092bc68d3745ad2e7f
ExequielMaydana <exedevcoding22@gmail.com>
2023-08-24 12:37:34 -0300

    validations form register volunteers

--- front/src/app/modules/auth/components/form-volunteerregister/form-volunteerregister.component.ts
+++ front/src/app/modules/auth/components/form-volunteerregister/form-volunteerregister.component.ts
@@ -17,6 +17,13 @@ export class FormVolunteerregisterComponent {
   showPassword: boolean = false;
   subscription: Subscription | null = null;
 
+  // Errors Validations
+  errorName: string = '';
+  errorEmail: string = '';
+  errorEmailTwo: string = '';
+  errorPhone: string = '';
+  errorPassword: string = '';
+
   constructor(private router: Router, private authService: AuthService) {}
 
   sendValues() {
@@ -27,15 +34,31 @@ export class FormVolunteerregisterComponent {
       password: this.password,
     };
 
-    this.authService.registerVolunteer(userData).subscribe({
-      next: (response) => {
-        console.log('Registro exitoso:', response);
-      },
-      error: (error) => {
-        console.error('Error en el registro:', error);
-      },
-      complete: () => {},
-    });
+    if (userData.fullName.length == 0) {
+      this.errorName = 'Por favor, ingresa un nombre válido.';
+    } else if (userData.email.length == 0) {
+      this.errorEmail =
+        'Por favor, ingresa una dirección de correo electrónico válida.';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(userData.email)
+    ) {
+      this.errorEmailTwo =
+        'Eso no parece una dirección de correo electrónico válida.';
+    } else if (userData.telefono.length == 0) {
+      this.errorPhone = 'Por favor, ingresa un teléfono válido.';
+    } else if (userData.password.length == 0) {
+      this.errorPassword = 'Por favor, ingresa una contraseña.';
+    } else {
+      this.authService.registerVolunteer(userData).subscribe({
+        next: (response) => {
+          console.log('Registro exitoso:', response);
+        },
+        error: (error) => {
+          console.error('Error en el registro:', error);
+        },
+        complete: () => {},
+      });
+    }
   }
 
   sendShowPassword() {
@@ -45,7 +68,7 @@ export class FormVolunteerregisterComponent {
   }
 
   navigateToLogin() {
-    this.router.navigate(['/login']);
+    this.router.navigate(['/auth/login']);
   }
 
   ngOnDestroy(): void {
