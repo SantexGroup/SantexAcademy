a9e377bd4a681ed08adc645f56c0255fcbdf4427
vani <you@example.com>
2023-08-24 15:49:26 -0300

    agregado:import

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputLoginComponent } from './modules/input-login/input-login.component';
-
+import { ReactiveFormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +13,10 @@ import { InputLoginComponent } from './modules/input-login/input-login.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
