f982dfc623fd8ca1a438dd36b0ffcc3c1749e9f1
ExequielMaydana <exedevcoding22@gmail.com>
2023-08-23 21:57:05 -0300

    refactoring front

--- front/src/app/shared/components/navbar/navbar.component.ts
+++ front/src/app/shared/components/navbar/navbar.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   isOpen: boolean = false;
+  openOptionOne: boolean = false;
+  openOptionTwo: boolean = false;
 
   constructor() {
     // Obtener el valor de isOpen desde el LocalStorage (si existe)
@@ -20,5 +22,20 @@ export class NavbarComponent {
     this.isOpen = !this.isOpen;
     // Guardar el valor actual de isOpen en el LocalStorage
     localStorage.setItem('isOpen', JSON.stringify(this.isOpen));
+  }
+
+  openOpOne() {
+    if (!this.openOptionOne) {
+      this.openOptionOne = true;
+    } else {
+      this.openOptionOne = false;
+    }
+  }
+  openOpTwo() {
+    if (!this.openOptionTwo) {
+      this.openOptionTwo = true;
+    } else {
+      this.openOptionTwo = false;
+    }
   }
 }
