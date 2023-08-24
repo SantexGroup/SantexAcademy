496766048f4d20e3d7e81d8a98ddd967c7094028
FededelRincon <delrincon@gmail.com>
2023-08-24 16:21:49 -0300

    fix landingPage as HomeComponent

--- front/src/app/modules/landing/landing.module.ts
+++ front/src/app/modules/landing/landing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+  ],
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    SharedModule
   ]
 })
 export class LandingModule { }
