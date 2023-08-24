31c5b4372e6afdef0f3fe929b32355176c7e4290
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-24 01:52:32 -0300

    feat: Add a new interface for course-detail

---
+++ front/src/app/core/interface/courses-detail.interface.ts
@@ -0,0 +1,9 @@
+export interface CoursesDetail {
+    id: number;
+    paragraph1: string;
+    paragraph2: string;
+    image1url: string;
+    image2ur2: string;
+    createAt: Date;
+    updateAt: Date;
+}
