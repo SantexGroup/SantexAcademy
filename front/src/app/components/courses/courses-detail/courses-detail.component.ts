ccaf746feb77b13eb8f40ddf4edd137c347ed5c4
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-24 01:58:57 -0300

    feat: Inject course-detail service Add a ngOnInit only when access to the courses detail view and obtains the response

--- front/src/app/components/courses/courses-detail/courses-detail.component.ts
+++ front/src/app/components/courses/courses-detail/courses-detail.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoursesDetailsService } from 'src/app/services/courses-details.service';
 
 @Component({
   selector: 'app-courses-detail',
   templateUrl: './courses-detail.component.html',
   styleUrls: ['./courses-detail.component.scss']
 })
-export class CoursesDetailComponent {
+export class CoursesDetailComponent implements OnInit{
 
+  constructor(public courseDetailService: CoursesDetailsService){}
+
+  ngOnInit(){
+    this.courseDetailService.getPosts().subscribe(
+      (response) => {
+        this.courseDetailService.courses = response
+        console.log("Esta es la respuesta ", response)
+      }, 
+      (error) => {
+        console.error(error)
+      }
+    )
+  }
 }
