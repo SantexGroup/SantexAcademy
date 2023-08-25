212b5100b26cd14c2525d2410efe7efc026fb262
cgavernet <carlosgavernet@gmail.com>
2023-08-25 10:58:22 -0300

    fix(api): Change foreignKey value in course model

--- back/models/coursedetail.js
+++ back/models/coursedetail.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       courseDetail.hasMany(models.Course, {
-        foreignKey: 'course_id',
+        foreignKey: 'idcoursedetail',
       });
     }
   }
