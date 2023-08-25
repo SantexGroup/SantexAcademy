8fbd45e62376c6b2e7087d18c93e3bd99fdec0c7
Arguello Soledad <soledad.ar1@gmail.com>
2023-08-25 00:03:18 -0300

    feat(courses-detail.ts): Add a new methods
    Add a new method for the components details.ts

--- front/src/app/components/courses/courses-detail/courses-detail.component.ts
+++ front/src/app/components/courses/courses-detail/courses-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CoursesDetailsService } from 'src/app/services/courses-details.service';
 
 @Component({
@@ -8,17 +10,36 @@ import { CoursesDetailsService } from 'src/app/services/courses-details.service'
 })
 export class CoursesDetailComponent implements OnInit{
 
-  constructor(public courseDetailService: CoursesDetailsService){}
+  constructor(public courseDetailService: CoursesDetailsService, private activateRoute: ActivatedRoute){}
+
+  subscription: Subscription | null = null;
 
   ngOnInit(){
-    this.courseDetailService.getPosts().subscribe(
+
+    const id = this.activateRoute.snapshot.params["id"]
+    let exists: boolean = false;
+    this.subscription = this.courseDetailService.getCoursesDetail(id).subscribe(
       (response) => {
-        this.courseDetailService.courses = response
+        this.courseDetailService.coursesDetail = response
         console.log("Esta es la respuesta ", response)
+        exists = true;
+        
       }, 
       (error) => {
         console.error(error)
       }
     )
   }
+
+  ngOnDestroy() {
+
+    if(this.subscription) {
+
+      this.subscription.unsubscribe();
+
+    }
+
+  }
+
 }
+
