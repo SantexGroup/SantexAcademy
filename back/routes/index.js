366336506ade017fc553bb5e6d436e4e4cf92bf2
PabloAndresBustos <pabloandresbustos@gmail.com>
2023-08-24 22:32:57 -0300

    aplicamos Angular Material a las opciones

--- back/routes/index.js
+++ back/routes/index.js
@@ -14,6 +14,8 @@ const referencesRoutes = require('./references.routes');
 const skillsRoutes = require('./skills.routes');
 const optinalsRoutes = require('./optional.routes');
 const contryRoutes = require('./countries.routes');
+const sexsRoutes = require('./sexs.routes');
+const maritalRoutes = require('./marital.routes');
 
 const app = Express();
 
@@ -28,6 +30,8 @@ app.use('/experiences', experienceRoutes);
 app.use('/references', referencesRoutes);
 app.use('/skills', skillsRoutes);
 app.use('/countries', contryRoutes);
+app.use('/gender', sexsRoutes);
+app.use('/marital', maritalRoutes);
 app.use('/', rootPath.handler);
 app.use(rootPath.setHeaders);
 app.use(errors.handler);
