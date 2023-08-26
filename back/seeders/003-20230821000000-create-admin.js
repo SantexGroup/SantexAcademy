c7b7e0a7c8aa1babc5e7f2eda3f882d6c0a93adc
Semper Evincere <spereyra.jus@gmail.com>
2023-08-26 19:27:22 -0300

    rama actualizada para mergear a solfront

--- back/seeders/003-20230821000000-create-admin.js
+++ back/seeders/003-20230821000000-create-admin.js
@@ -4,15 +4,15 @@ const faker = require('faker');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    const hashedPassword = await bcrypt.hash('passAdmin', 10);
+    const hashedPassword = await bcrypt.hash('admin123', 10);
 
     await queryInterface.bulkInsert('Users', [
       {
-        firstName: 'Sergio',
-        lastName: 'Pereyra',
-        userName: 'semper_admin',
+        firstName: 'Administrador',
+        lastName: 'ApellidoDelAdministrador',
+        userName: 'mixx',
         password: hashedPassword,
-        email: 'elsemper@gmail.com',
+        email: 'admin@gmail.com',
         rol: 'Admin',
         phone: faker.phone.phoneNumber(),
         createdAt: new Date(),
@@ -22,7 +22,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    // Elimina el usuario administrador
-    await queryInterface.bulkDelete('Users', { userName: 'semper_admin' });
+    await queryInterface.bulkDelete('Users', { userName: 'mixx' });
   },
 };
