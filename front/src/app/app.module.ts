2d8a2d66c345c3eb0141ea14f9739ee23222c366
Pablo Medina <pablomedina2493@gmail.com>
2023-08-25 21:36:50 -0300

    Inicio Sesion-Login

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- front/src/app/modules/dashboard/dashboard-page/dashboard-page.component.ts
+++ front/src/app/modules/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.css']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit{
+  userLoginOn:boolean=false;
   constructor() {}
+
+
+  ngOnInit(): void {
+
+  }
+
 
 }
