a2ad7beb94dad0a522bab4a03dcdb8daaf6c4d70
PabloAndresBustos <pabloandresbustos@gmail.com>
2023-08-27 20:18:55 -0300

    Configuaraciones de Optionals

--- front/src/app/modules/optionals/optionals.component.ts
+++ front/src/app/modules/optionals/optionals.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Countries } from 'src/app/interfaces/country.interface';
 import { Marital } from 'src/app/interfaces/marital.interface';
+import { Optionals } from 'src/app/interfaces/optionlas.interface';
 import { Sexs } from 'src/app/interfaces/sex.interface';
 import { CountriesService } from 'src/app/services/countries.service';
 import { GenderService } from 'src/app/services/gender.service';
 import { MaritalsService } from 'src/app/services/maritals.service';
+import { OptionalsService } from 'src/app/services/optionals.service';
 
 @Component({
   selector: 'app-optionals',
@@ -13,11 +16,41 @@ import { MaritalsService } from 'src/app/services/maritals.service';
 })
 export class OptionalsComponent implements OnInit {
 
+  ngOnInit(): void {
+
+    this.getListCountries();
+
+    this.getListGender();
+
+    this.getListMaritals();
+
+  }
+
+  optionalsForm: FormGroup;
+
   constructor(
     private _countriesService: CountriesService,
     private _maritalsService: MaritalsService,
-    private _genderServices: GenderService
-    ) {}
+    private _genderServices: GenderService,
+    private _optionalsService: OptionalsService,
+    private fb: FormBuilder
+    ) {
+      this.optionalsForm = this.fb.group({
+        maritalId: [''],
+        sexsId: [''],
+        countriesId: [''],
+        profile: [''],
+        webPage: [''],
+        linkedIn: [''],
+        hobbies: [''],
+        aptitudes: [''],
+        driverLicense: [''],
+        aboutMe: [''],
+        achievements: [''],
+        address: [''],
+        zipCode: [''],
+      });
+    }
 
   //Countries
   getListCountries(){
@@ -25,7 +58,7 @@ export class OptionalsComponent implements OnInit {
       this.countries = countriesList;
     });
   }
-
+  
   countries: Countries[] = [];
 
   //Maritals
@@ -46,12 +79,25 @@ export class OptionalsComponent implements OnInit {
 
   gender: Sexs[] = [];
 
- 
-  ngOnInit(): void {
-    this.getListCountries();
+  postOptionals(){
+    const newOptionals: Optionals = {
+      maritalId: this.optionalsForm.get('maritalId')?.value,
+      sexsId: this.optionalsForm.get('sexsId')?.value,
+      countriesId: this.optionalsForm.get('countriesId')?.value,
+      profile: this.optionalsForm.get('profile')?.value,
+      webPage: this.optionalsForm.get('webPage')?.value,
+      linkedIn: this.optionalsForm.get('linkedIn')?.value,
+      hobbies: this.optionalsForm.get('hobbies')?.value,
+      aptitudes: this.optionalsForm.get('aptitudes')?.value,
+      driverLicense: this.optionalsForm.get('driverLicense')?.value,
+      aboutMe: this.optionalsForm.get('aboutMe')?.value,
+      achievements: this.optionalsForm.get('achievements')?.value,
+      address: this.optionalsForm.get('address')?.value,
+      zipCode: this.optionalsForm.get('zipCode')?.value,
+    }
 
-    this.getListGender();
-
-    this.getListMaritals();
+    this._optionalsService.addOptionals(newOptionals).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
