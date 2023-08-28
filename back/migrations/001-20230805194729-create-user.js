7e82b3df20c6dc5c629b8843da0ad80253915972
Semper Evincere <spereyra.jus@gmail.com>
2023-08-28 12:11:24 -0300

    seeders de encuestas actualizado con respuestas coherentes

---
+++ back/migrations/001-20230805194729-create-user.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      userName: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      rol: {
+        type: Sequelize.STRING,
+      },
+      phone: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
--- back/seeders/002-20230813120000-create-surveys.js
+++ back/seeders/002-20230813120000-create-surveys.js
@@ -1,34 +1,14 @@
 /* eslint-disable no-plusplus */
 const faker = require('faker');
+const { questions } = require('./data/questionsAndAnswers');
 
-// Genera una respuesta aleatoria para cada pregunta
 const generateRandomAnswers = () => {
-  const questions = [
-    '¿Cómo te enteraste de Mina Clavero como destino turístico?',
-    '¿Qué te motivó a elegir Mina Clavero para pasar tus vacaciones?',
-    '¿Qué tipo de hospedaje elegiste durante tu estadía en Mina Clavero?',
-    '¿Cuál es el propósito de tu visita a Mina Clavero?',
-    '¿Cuánto tiempo planeas quedarte en Mina Clavero?',
-    '¿Qué actividades planeas realizar durante tu estadía en Mina Clavero?',
-    '¿Qué lugares planeas visitar durante tu estadía en Mina Clavero?',
-    '¿Qué tipo de transporte utilizaste para llegar a Mina Clavero?',
-    '¿Qué medio de transporte utilizaste para moverte dentro de Mina Clavero?',
-    '¿Cómo calificarías la calidad de los servicios turísticos en Mina Clavero?',
-    '¿Cómo te comunicaste con las empresas turísticas en Mina Clavero?',
-    '¿Qué tipo de material se te entregó en la Oficina de Turismo?',
-    '¿Cómo evalúas el tipo de información brindada por las Oficinas de Turismo de Mina Clavero?',
-    '¿Aparte de la información solicitada, se te brindó algún otro tipo de información de tu interés?',
-    '¿Qué información se te brindó?',
-    '¿Consideras a Mina Clavero como un destino completo en cuanto a su oferta y servicios?',
-    '¿Recomendarías Mina Clavero como destino turístico?',
-  ];
-
   const answers = {};
-  questions.forEach((question) => {
-    const randomAnswer = faker.lorem.sentence();
-    answers[question] = randomAnswer;
+  Object.keys(questions).forEach((question) => {
+    const options = questions[question];
+    const randomOption = faker.random.arrayElement(options);
+    answers[question] = randomOption;
   });
-
   return answers;
 };
 
