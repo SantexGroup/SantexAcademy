90016a9e899667c97af1e421b48f65a6b358870d
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-08-29 20:29:34 -0300

    creado: guard para proteger las vistas de admin

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardAdminComponent } from './modules/dashboard-admin/dashboard-admin.component';
+import { PermissionsAdminGuard } from './core/guards/permissions-admin.guard';
 
 const routes: Routes = [
   {
     path: 'dashboard-admin',
     component: DashboardAdminComponent,
+    canActivate: [PermissionsAdminGuard]
   },
+
+  //Este path debe ir siempre al final para que redirija a dashboard-admin cuando el user ingrese una ruta inexistente
   {
     path: '**',
     redirectTo: 'dashboard-admin'
