b0cc180c7d137f9cfca8dfc58922b1bfcf788660
Nievas2 <angelgabrielnievas@gmail.com>
2023-08-29 12:47:37 -0300

    unnecessary changes out

--- back/middleware/root_path.middleware.js
+++ back/middleware/root_path.middleware.js
@@ -5,21 +5,17 @@
  */
 function handler(req, res) {
   res.statusCode = 404;
-  res.json({ error: "not_found", message: "resource not found" });
+  res.json({ error: 'not_found', message: 'resource not found' });
 }
 module.exports.handler = handler;
 
 function setHeaders(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, Authorization, Content-Type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers',
+    'X-Requested-With, content-type, Authorization, Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 }
 module.exports.setHeaders = setHeaders;
--- back/models/index.js
+++ back/models/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable global-require */
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const Course =require("./course")
+const CourseCategory=require("./coursecategory")
+const Schedule =require("./schedule")
+
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 // eslint-disable-next-line import/no-dynamic-require
 const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
@@ -13,25 +17,15 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs.readdirSync(__dirname)
-  .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-  )
+fs
+  .readdirSync(__dirname)
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     // eslint-disable-next-line import/no-dynamic-require
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
@@ -43,4 +37,5 @@ Object.keys(db).forEach((modelName) => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-module.exports = db;
+module.exports =db
+
