20623ae9a1dbf7c96fd5d9b21b7da7ddebbb4b37
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-08-29 20:02:52 -0300

    Se creó el middleware isAuthenticatedAdmin

--- back/middleware/authentication.js
+++ back/middleware/authentication.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 
 const isAuthenticatedVoluntario = async (req, res, next) => {
   try {
+    // eslint-disable-next-line consistent-return
     await passport.authenticate('jwt', { session: false }, (err, user, info) => {
       if (err || !user || info.tipo !== 'voluntario') {
         const error = new Error('Usuario no autorizado');
@@ -18,6 +19,7 @@ const isAuthenticatedVoluntario = async (req, res, next) => {
 
 const isAuthenticatedOrganizacion = async (req, res, next) => {
   try {
+    // eslint-disable-next-line consistent-return
     await passport.authenticate('jwt', { session: false }, (err, user, info) => {
       if (err || !user || info.tipo !== 'organizacion') {
         const error = new Error('Usuario no autorizado');
@@ -32,4 +34,21 @@ const isAuthenticatedOrganizacion = async (req, res, next) => {
   }
 };
 
-module.exports = { isAuthenticatedVoluntario, isAuthenticatedOrganizacion };
+const isAuthenticatedAdmin = async (req, res, next) => {
+  try {
+    // eslint-disable-next-line consistent-return
+    await passport.authenticate('jwt', { session: false }, (err, user, info) => {
+      if (err || !user || info.tipo !== 'admin') {
+        const error = new Error('Usuario no autorizado');
+        error.status = 401;
+        return next(error);
+      }
+      req.usuario = user;
+      next();
+    })(req, res, next);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { isAuthenticatedVoluntario, isAuthenticatedOrganizacion, isAuthenticatedAdmin };
