adb677aa1d6a75bef8f0e01d541dd4756574bbc9
Nievas2 <angelgabrielnievas@gmail.com>
2023-08-30 11:38:13 -0300

    components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminPanelComponent } from './modules/admin-panel/admin-panel.component';
+import { EditCourseComponent } from './modules/admin-panel/edit-course/edit-course.component';
+import { AddCourseComponent } from './modules/admin-panel/add-course/add-course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPanelComponent,
+    EditCourseComponent,
+    AddCourseComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/modules/admin-panel/add-course/add-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-course',
+  templateUrl: './add-course.component.html',
+  styleUrls: ['./add-course.component.css']
+})
+export class AddCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
