b6faa94791ade0eb3a8007265c091c8bcbfa2770
Gerardo Fabian Quispe <gfq0684@gmail.com>
2023-08-31 19:05:44 -0300

    borramos algunas lineas de codigo y comentarios

--- front/src/app/modules/dashboard/nav-bar/nav-bar.module.ts
+++ front/src/app/modules/dashboard/nav-bar/nav-bar.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar.component';
 import { NavbarRoutingModule } from './nav-bar-routing.module';
 
 
