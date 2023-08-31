2a543192b89bf03b829c5e777e42480ecaa22906
HectorB <elmillo1509@gmail.com>
2023-08-31 09:33:09 -0300

    fix(api): resolved migration courseDetail problem change coursedetail to courseDetail in 20230817120514-create-course-detail.js

--- back/migrations/20230817120514-create-course-detail.js
+++ back/migrations/20230817120514-create-course-detail.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('coursedetails', {
+    await queryInterface.createTable('courseDetails', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,6 +42,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('coursedetails');
+    await queryInterface.dropTable('courseDetails');
   }
 };
