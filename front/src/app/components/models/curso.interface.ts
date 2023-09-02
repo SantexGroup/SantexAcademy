29f77e65539433bd7928d6b672f3b0f3eadea5d4
Eduardo-Villella <eduardovillella@yahoo.com>
2023-09-02 00:15:28 -0300

    sinErrores_cambioTemporaldeCursosModel_preJuanjo

--- front/src/app/components/models/curso.interface.ts
+++ front/src/app/components/models/curso.interface.ts
@@ -5,22 +5,26 @@
 //   const curso = Convert.toCurso(json);
 
 export interface Curso {
-  id:                    number;
-  nombre:                string;
-  descripcion:           string;
-  imagen:                string;
-  duracion:              number;
-  capacidad:             number;
-  idnivel:               null;
-  requisitos:            string;
-  habilitado:            boolean;
-  fechaInicio:           Date;
-  fechafin:              null;
-  idusuarioalta:         null;
-  estado:                boolean;
-  idusuariomodificacion: null;
-  createdAt:             Date;
-  updatedAt:             Date;
+  // id:                    number;
+  // nombre:                string;
+  // descripcion:           string;
+  // imagen:                string;
+  // duracion:              number;
+  // capacidad:             number;
+  // idnivel:               null;
+  // requisitos:            string;
+  // habilitado:            boolean;
+  // fechaInicio:           Date;
+  // fechafin:              null;
+  // idusuarioalta:         null;
+  // estado:                boolean;
+  // idusuariomodificacion: null;
+  // createdAt:             Date;
+  // updatedAt:             Date;
+  id: string;//Eliminar cuando se implemente DB
+  name: string;//Eliminar cuando se implemente DB
+  price: number;//Eliminar cuando se implemente DB
+  image: string;//Eliminar cuando se implemente DB
 }
 
 // Converts JSON strings to/from your types
