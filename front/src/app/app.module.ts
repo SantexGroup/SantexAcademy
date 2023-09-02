8e518b0710b47ecefb34612913d08a2bc1f69734
pablo-maroto <pabloemaroto@gmail.com>
2023-09-02 10:16:50 -0300

    HU4-Agregado pantalla de registro de usuario

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,15 +6,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BuyerRegistrationComponent } from './buyer-registration/buyer-registration.component';
-import { SellerRegistrationComponent } from './seller-registration/seller-registration.component';
+import { BuyerRegistrationComponent } from './component/buyer-registration/buyer-registration.component';
+
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuyerRegistrationComponent,
-    SellerRegistrationComponent,
   ],
   imports: [
     BrowserModule,
