179162d6a7a7af049636c570f3f2abc3b8a559d9
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-02 21:06:20 -0300

    Se creó el componente tareas-organizacion

--- front/src/app/modules/organizaciones/organizaciones.module.ts
+++ front/src/app/modules/organizaciones/organizaciones.module.ts
@@ -6,13 +6,15 @@ import { OrganizacionesComponent } from './organizaciones.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { DashboardOrganizacionComponent } from './dashboard-organizacion/dashboard-organizacion.component';
 import { DatosOrganizacionComponent } from './datos-organizacion/datos-organizacion.component';
+import { TareasOrganizacionComponent } from './tareas-organizacion/tareas-organizacion.component';
 
 
 @NgModule({
   declarations: [
     OrganizacionesComponent,
     DashboardOrganizacionComponent,
-    DatosOrganizacionComponent
+    DatosOrganizacionComponent,
+    TareasOrganizacionComponent
   ],
   imports: [
     CommonModule,
---
+++ front/src/app/modules/organizaciones/tareas-organizacion/tareas-organizacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas-organizacion',
+  templateUrl: './tareas-organizacion.component.html',
+  styleUrls: ['./tareas-organizacion.component.css']
+})
+export class TareasOrganizacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
