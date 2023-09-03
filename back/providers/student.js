4a772b8b8a96b56c3efec715b2745ba7804f5193
HectorB <elmillo1509@gmail.com>
2023-09-02 21:32:10 -0300

    feat(api): Student, updateStudent now supports updating related entities
    This commit enhances the `updateStudent` method to allow updates to related entities such as ContactInformation, User, and Cohorts. When updating a student, you can now provide changes to these related entities within the student object. The method handles updates for each related entity individually.
    
    - Update student data with Student.update.
    - Update ContactInformation if provided in the student object.
    - Update User information if provided in the student object.
    - Update associated cohorts, replacing existing associations with new ones if provided.
    
    This change improves the flexibility of the `updateStudent` method and allows for more comprehensive updates to student records and their related data.

--- back/providers/student.js
+++ back/providers/student.js
@@ -74,11 +74,40 @@ const assignCohortToStudent = async (studentId, cohortId) => {
 
 const updateStudent = async (id, student) => {
   try {
+    // Actualizams el estudiante
     const [updatedStudentCount] = await Student.update(student, {
       where: {
         id,
       },
     });
+
+    // Luego, si es necesario actualizamos la información de contacto
+    if (student.ContactInformation) {
+      await ContactInformation.update(student.ContactInformation, {
+        where: {
+          id: student.id_contact_information,
+        },
+      });
+    }
+    // Y el usuario
+    if (student.User) {
+      await User.update(student.User, {
+        where: {
+          id: student.id_user,
+        },
+      });
+    }
+
+    if (student.Cohorts) {
+      // Si deseamos modificar multiples cohorts, podemos hacerlo de esta manera:
+      // Removemos la asociación de los cohorts existentes con el estudiante
+      // Generamos una nueva
+      const studentInstance = await Student.findByPk(id);
+
+      // Reemplazamos con los cohort nuevos
+      await studentInstance.setCohorts(student.Cohorts.map((cohort) => cohort.id));
+    }
+
     return updatedStudentCount;
   } catch (error) {
     throw new Error('Error al actualizar el estudiante');
