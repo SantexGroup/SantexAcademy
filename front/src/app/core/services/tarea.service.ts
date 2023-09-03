0fbdf27b5c4ea17bc34a700c6b76a9d6f6740e48
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-03 18:15:54 -0300

    Se creó el método crearTarea en el servicio de Tarea

--- front/src/app/core/services/tarea.service.ts
+++ front/src/app/core/services/tarea.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../http/api.service';
+import { Observable } from 'rxjs';
+import { Tarea } from '../interfaces/tarea';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,7 @@ export class TareaService {
 
   constructor(private apiService:ApiService) { }
 
-  
+  crearTarea(nuevaTarea:Tarea):Observable<Tarea>{
+    return this.apiService.post('/tarea/create-tarea', nuevaTarea);
+  }
 }
