98d279cedcaaf4df3b086e841484d14461248f80
FedeBarrera <fede94barrera@gmail.com>
2023-09-04 13:54:02 -0300

    corrección de errores

--- front/src/app/core/interfaces/courseCategory.ts
+++ front/src/app/core/interfaces/courseCategory.ts
@@ -1,4 +1,4 @@
 export interface CourseCategory{
-    id?: number;
+    id: number;
     name: string;
 }
--- front/src/app/core/interfaces/schedule.ts
+++ front/src/app/core/interfaces/schedule.ts
@@ -1,5 +1,5 @@
 export interface Schedule{
-    id?: number;
+    id: number;
     active: boolean;
     where: string,
     course: string,
