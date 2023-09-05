640ed0946152c7db4c198da276f98aa646b0ee8a
Sebastian Ledesma <sevaledesma@gmail.com>
2023-09-04 23:21:30 -0300

    Validacion login

--- front/src/app/modules/login/components/login/login.component.ts
+++ front/src/app/modules/login/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../../services/auth.service'; 
+import { AuthService } from '../../../../services/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,21 +12,24 @@ export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-  login() {
-    this.authService.login(this.email, this.password).subscribe(
-      (response: any) => {
-        console.log('Inicio de sesión exitoso', response);
-        // Maneja la respuesta aquí, por ejemplo, almacena el token en el local storage
-      },
-      (error: any) => {
-        console.error('Error al iniciar sesión:', error);
-      }
-    );
+  login(loginForm: NgForm) {
+    if (loginForm.valid) {
+      const email = this.email; 
+      const password = this.password; 
+
+      this.authService.login(email, password).subscribe(
+        (response: any) => {
+          console.log('Inicio de sesión exitoso', response);
+          // Manejar la respuesta para almacenar el token en el local storage
+        },
+        (error: any) => {
+          console.error('Error al iniciar sesión:', error);
+        }
+      );
+    }
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
