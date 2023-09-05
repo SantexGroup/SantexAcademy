0807182ca347807de98e1b1d1a50e6e6c8e72543
AndreeRdz <rodriguez.andree87@gmail.com>
2023-09-05 14:53:50 -0500

    Login-Angular

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginViewComponent}from './modules/login-view/login-view.component';
+import{SigupViewComponent} from './modules/sigup-view/sigup-view.component'
 
 const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+  },
+  {
+    path: 'login',
+    component: LoginViewComponent,
+  },
+  {
+    path: 'signup',
+    component: SigupViewComponent,
   },
   {
     path: '**',
---
+++ front/src/app/modules/sigup-view/sigup-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sigup-view',
+  templateUrl: './sigup-view.component.html',
+  styleUrls: ['./sigup-view.component.css']
+})
+export class SigupViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+}
