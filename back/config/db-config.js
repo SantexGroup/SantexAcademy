59c07a654a2ec6e3c26c047a4b449f16b268bdb0
MauriDemasi <mauridemasi88@gmail.com>
2023-09-06 00:44:18 -0300

    first test on deploy

--- back/config/db-config.js
+++ back/config/db-config.js
@@ -2,25 +2,15 @@ const { Sequelize } = require("sequelize"); // Take the Sequelize Class
 require("dotenv").config();
 
 
-// Se crea la instancia de sequelize con los datos de conexión a la base de datos en el CONSTRUCTOR
-// Se puede usar una sola variable de entorno DB_URL que contenga todos los datos necesarios
-// O se pueden usar variables separadas para el nombre de la base de datos, el usuario, la contraseña y el host
-// El dialecto se puede especificar como una opción o se puede inferir de la URL
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: "mysql",
+const url = require('url');
+const dbUrl = process.env.DB_URL;
+const dbConfig = url.parse(dbUrl);
+const sequelize = new Sequelize(dbConfig.path.substring(1), dbConfig.auth.split(':')[0], dbConfig.auth.split(':')[1], {
+  host: dbConfig.host.split(':')[0],
+  port: dbConfig.host.split(':')[1],
+  dialect: 'mysql'
 });
 
-// Se prueba la conexión con el método authenticate
-// Se usa una función asíncrona para esperar la promesa
-// Se usa try-catch para manejar los posibles errores
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('La conexión se ha establecido correctamente.');
-  } catch (error) {
-    console.error('No se ha podido conectar a la base de datos:', error);
-  }
-})();
 
 
 const initializeDB = async () => {
