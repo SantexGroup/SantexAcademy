214a709976ea6240e30746c9f11f550cfc173f58
Exequiel <eportero421@gmail.com>
2023-09-06 03:48:39 -0300

    chore(wip): Fix course completion date calc
    Mark the 'courses.service.ts' as Work in Progress (WIP) t address
    incorrect date calculation for course completion. Also, note the
    harcoded data in the course-detail.component.html in the paragraph of
    start_date and end_date

--- front/src/app/services/courses.service.ts
+++ front/src/app/services/courses.service.ts
@@ -10,9 +10,13 @@ export class CoursesService {
 
   courses: any = [];
 
+  fechaFinalizacion: Date;
+
   private endpoint = 'http://localhost:4001/courses'; //creamos una variable para el endpoint de los cursos
 
-  constructor(private http: HttpClient) {} //creamos una variable para el modulo de HttpClient
+  constructor(private http: HttpClient) {
+    this.fechaFinalizacion = new Date();
+  } //creamos una variable para el modulo de HttpClient
 
   getPosts():Observable<Course[]> { //creamos un metodo que va a devolver un observable con el array de cursos
 
@@ -32,6 +36,28 @@ export class CoursesService {
   getRoundedPrice(price: number, due: number): number { //este metodo me redondea el valor de la cuota
 
     return Math.round(price / due); //me redondea el precio que se divide por la cuota
+
+  }
+
+  getEndDate() {
+
+    const startDate = new Date(this.courses.start_date);
+    const duration = this.courses.duration;
+
+  if (!isNaN(startDate.getTime()) && !isNaN(duration)) {
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + duration);
+
+    console.log('startDate:', startDate);
+    console.log('duration:', duration);
+    console.log('endDate:', endDate);
+
+    this.fechaFinalizacion = endDate;
+    return this.fechaFinalizacion;
+  } else {
+    console.error('Valores de fecha o duración no válidos.');
+    return null; // O puedes devolver un valor por defecto o lanzar una excepción según tus necesidades
+  }
 
   }
 
