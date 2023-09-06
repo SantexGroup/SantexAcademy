289bccf7665bf72c60140af0eb7faab17ef252b6
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-06 14:47:39 -0300

    Se agregó un menú, y un botón para modificar el estado de la tarea

--- front/src/app/modules/organizaciones/tareas-organizacion/tareas-organizacion.component.ts
+++ front/src/app/modules/organizaciones/tareas-organizacion/tareas-organizacion.component.ts
@@ -17,7 +17,7 @@ export class TareasOrganizacionComponent implements OnInit {
   constructor(private dialog:MatDialog, private tareaService:TareaService) { }
 
   
-  columnasTabla: string[] = ['nombre','fecha','acciones'];
+  columnasTabla: string[] = ['nombre','estado','acciones'];
   dataSource = new MatTableDataSource();
 
   ngOnInit(): void {
@@ -98,6 +98,48 @@ export class TareasOrganizacionComponent implements OnInit {
             Swal.fire(
               'Error',
               'No se pudo eliminar la tarea.',
+              'error'
+            );
+          }
+        });
+        
+      }
+    })
+  }
+
+  cambiarEstado(tarea:Tarea):void{
+    
+    const nuevoEstado:string = tarea.estado === 'activa'?'Finalizada':'Activa';
+    
+    Swal.fire({
+      title: 'Estas seguro/a?',
+      text: `Quieres modificar el estado de la tarea a: ${nuevoEstado}?`,
+      icon: 'warning',
+      iconColor:'#d33',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Si, modificar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        this.tareaService.modificarEstado(tarea.id!, nuevoEstado.toLocaleLowerCase()).subscribe({
+          next:()=>{
+               
+            Swal.fire(
+              'Modificado!',
+              `La tarea ahora tiene un estado de ${nuevoEstado}.`,
+              'success'
+            );
+            this.mostrarTareas();
+          
+          },
+          error:(err)=>{
+            console.log(err);
+            Swal.fire(
+              'Error',
+              'No se pudo modificar el estado.',
               'error'
             );
           }
