0dfc49897c7b875620aee12124644b61e3f5daf1
Martin Fernandez <fernandezmartin.k26@gmail.com>
2023-09-07 08:39:51 -0300

    Creando carpeta components

--- front/src/app/components/main-component/app-routing.module.ts
+++ front/src/app/components/main-component/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
-import { LoginComponent } from './login/login.component';
+import { FormularioRegistroComponent } from '../formulario-registro/formulario-registro.component';
+import { LoginComponent } from '../login/login.component';
 
 
 
@@ -12,7 +12,7 @@ const routes: Routes = [
 
   {
     path: 'dashboard',
-    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+    loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule),
   },
   {
     path: '**',
--- front/src/app/components/main-component/app.module.ts
+++ front/src/app/components/main-component/app.module.ts
@@ -8,14 +8,14 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import {MatCardModule} from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
+import { FormularioRegistroComponent } from '../formulario-registro/formulario-registro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from '../login/login.component';
 
 @NgModule({
   declarations: [
