82067f164fe25a9a184aea2be31fefb221ef333a
Arguello Soledad <soledad.ar1@gmail.com>
2023-09-07 13:33:34 -0300

    feat(courses): New components Create mock components file for form record courses file

---
+++ front/src/app/components/courses/courses-mock/mock-unsuccessfully.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-unsuccessfully',
+  templateUrl: './mock-unsuccessfully.component.html',
+  styleUrls: ['./mock-unsuccessfully.component.scss']
+})
+export class MockUnsuccessfullyComponent {
+
+}
--- front/src/app/components/pay-transf-course.module.ts
+++ front/src/app/components/pay-transf-course.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PayTransfCourseComponent } from './courses/pay-transf-course/pay-transf-course.component';
+import { MockUnsuccessfullyComponent } from './courses/courses-mock/mock-unsuccessfully.component';
+import { MockSuccessComponent } from './courses/courses-mock/mock-success.component';
 
 
 
@@ -8,7 +10,16 @@ const routes: Routes = [
   {
     path: '',
     component: PayTransfCourseComponent
+  },
+  {
+    path: 'unseccessfully',
+    component: MockUnsuccessfullyComponent
+  },
+  {
+    path: 'success',
+    component: MockSuccessComponent
   }
+
 ]
 
 @NgModule({
