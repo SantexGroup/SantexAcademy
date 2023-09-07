b75ba751b5c38665590cd4a5254682bb97aa305e
Javier Lencina <carlosjavier.lencina@gmail.com>
2023-09-07 03:22:08 -0300

    Se continua con la configuracion del componente

--- front/src/app/modules/crud-data/personal/personal.component.ts
+++ front/src/app/modules/crud-data/personal/personal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { UserDataService } from 'src/app/core/services/toolServices/userData.service';
 
 @Component({
   selector: 'app-personal',
@@ -12,15 +13,17 @@ export class PersonalComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
+    private userData: UserDataService
   ) { }
 
   ngOnInit() {
     console.log("desde personal")
-    this.route.paramMap.subscribe(params => {
+    /* this.route.paramMap.subscribe(params => {
       if (params.has('id')) {
         console.log(params.get('id'));
       }      
-    })
+    }) */
+    console.log(this.userData.userId) 
   }
 
 
