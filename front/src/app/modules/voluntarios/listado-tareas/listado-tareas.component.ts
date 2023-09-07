3ee3fd14000bd24d17615274d75ca96ee538fb0f
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-07 20:35:32 -0300

    Se muestran las tareas en componente listado-tareas

--- front/src/app/modules/voluntarios/listado-tareas/listado-tareas.component.ts
+++ front/src/app/modules/voluntarios/listado-tareas/listado-tareas.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Tarea } from 'src/app/core/interfaces/tarea';
+import { TareaService } from 'src/app/core/services/tarea.service';
+import { DetalleTareaComponent } from '../../organizaciones/modales-organizacion/detalle-tarea/detalle-tarea.component';
 
 @Component({
   selector: 'app-listado-tareas',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoTareasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tareasService:TareaService, private dialog:MatDialog) { }
 
   ngOnInit(): void {
+    this.mostrarTareas();
   }
 
+  listadoTareas:Tarea[] = [];
+
+  mostrarTareas():void{
+    this.tareasService.getTareas().subscribe({
+      next:(res)=>{
+        this.listadoTareas = res;
+      },
+      error:()=>{
+      }
+    });
+  }
+
+  mostrarDetalles(tarea:Tarea):void{
+    this.dialog.open(DetalleTareaComponent,{data:tarea});
+  }
+  
 }
