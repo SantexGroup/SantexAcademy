18b4db0e61b6b821936c1bfb613059255d64b295
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-07 18:01:01 -0300

    Se creó el componente listado-tareas

---
+++ front/src/app/modules/voluntarios/listado-tareas/listado-tareas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-tareas',
+  templateUrl: './listado-tareas.component.html',
+  styleUrls: ['./listado-tareas.component.css']
+})
+export class ListadoTareasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
