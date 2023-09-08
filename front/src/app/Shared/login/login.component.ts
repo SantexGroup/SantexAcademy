e98cd55937ba579feaca65812ab1d44e99f6f1d4
tadeoressio <tadeo.ressio@gmail.com>
2023-09-08 18:30:30 -0300

    Logeo y persistencia rehecho según parse en localStorage. Queda pendiente hacer el cambio a estado vendedor

--- front/src/app/Shared/login/login.component.ts
+++ front/src/app/Shared/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   logeadoComprador: boolean = false;
   logeadoVendedor: boolean = false;
   usuarioLogeado: boolean = false;
-  infoLocal: any[] = [];
+  // infoLocal: any[] = [];
   // infoLocal: any;
   // infoLocal: string = '';
   resLogin: any[] = [];
@@ -29,63 +29,158 @@ export class LoginComponent implements OnInit {
   constructor(private service: LoginService, private router: Router) { } 
 
   ngOnInit(): void {
-    // this.infoLocal = localStorage.getItem(JSON.parse('resLog'));
-    const tokenLocal = localStorage.getItem('token');
-    // this.infoLocal = localStorage.getItem('resLog');
-    const venLocal = localStorage.getItem('estadoDeVendedor');
-    // const venLocal = localStorage.getItem('estadoDeVendedor');
-    console.log(tokenLocal)
-    console.log(venLocal)    
-    // console.log(this.infoLocal[1].users.dni);
-    if (tokenLocal && venLocal == 'false') {
-      this.logeadoComprador = true;
-      this.logeadoVendedor = false;
-    }
-    if (tokenLocal && venLocal == 'true') {
-      this.logeadoVendedor = true;
-      this.logeadoComprador = false;
-    }
+    this.corroborarLogeo();    
+  }
 
-    // let newObject = window.localStorage.getItem("resLog");
-    // console.log(JSON.parse(this.newObject));
-
-
-    // const newObject = localStorage.getItem('token');
-    // const tokenLoc = localStorage.getItem(JSON.parse('resLog'))
-    // console.log(tokenLoc)
-    // console.log('tokenLoc2', JSON.parse(tokenLoc))
-
-    /*
-    if (infoLocal && !infoLocal.users.estadoDeVendedor) {
-      this.logeadoComprador = true;
+  corroborarLogeo() {    
+    let infoLocal = localStorage.getItem('resLog')
+    if (infoLocal) {
+      let newObject = JSON.parse(infoLocal);
+      if (newObject) {
+        if (newObject[1].users.estadoDeVendedor) {
+          this.logeadoVendedor = true;
+          this.logeadoComprador = false;
+        }
+        if (!newObject[1].users.estadoDeVendedor) {
+          this.logeadoComprador = true;
+          this.logeadoVendedor = false;
+        }
+      }
     }
-    if (infoLocal && infoLocal.users.estadoDeVendedor) {
-      this.logeadoVendedor = true;
+  }
+
+
+  deslogear() {
+    let infoLocal = localStorage.getItem('resLog')
+    if (infoLocal) {
+      if (this.logeadoComprador || this.logeadoVendedor) {
+        this.logeadoVendedor = false;
+        this.logeadoComprador = false;
+        localStorage.clear()
+        this.router.navigateByUrl('/'); 
+        this.router.navigate(['/']);
+      }      
     }
-    */
-    
   }
   
+   
+  
+
   botonLogin() {
-    if (this.infoLocal) {
-      localStorage.clear()
+    let infoLocal = localStorage.getItem('resLog')
+    if (infoLocal) {
+      localStorage.clear();
     }
     this.service.login(this.corLog, this.pasLog).subscribe(res => {
       if (res) {
         localStorage.setItem( 'resLog', JSON.stringify(res));
-        localStorage.setItem( 'token', JSON.stringify(res[0].token));
-        localStorage.setItem( 'estadoDeVendedor', JSON.stringify(res[1].users.estadoDeVendedor));
-        localStorage.setItem( 'id', JSON.stringify(res[1].users.id));
         if (!res[1].users.estadoDeVendedor) {
           this.logeadoComprador = true;
+          this.logeadoVendedor = false;
         }
         if (res[1].users.estadoDeVendedor) {
           this.logeadoVendedor = true;
+          this.logeadoComprador = false;
         }
         this.router.navigateByUrl('/'); 
       }
     })
   }
+  
+  botonVendedor() {
+    let infoLocal = localStorage.getItem('resLog')
+    if (infoLocal && (this.logeadoComprador == true || this.logeadoVendedor == true)) {
+      let newObject = JSON.parse(infoLocal);
+      if (newObject) {
+        if (!newObject[1].users.estadoDeVendedor) {
+          const id = newObject[1].users.id;
+          const estadoVen = newObject[1].users.estadoDeVendedor;
+          let user = {id, estadoVen}
+          const cambioVendedor = this.service.cambioVendedorServ(user).subscribe(res => {
+            if (cambioVendedor) {
+              localStorage.clear();
+              localStorage.setItem( "token", JSON.stringify(res));
+              this.logeadoVendedor = true;
+              this.router.navigateByUrl('/');
+            }
+          })     
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+
+   // botonVendedor() {
+  //   const tokenLocal = localStorage.getItem('token');
+  //   const venLocal = localStorage.getItem('estadoDeVendedor');
+  //   const id = localStorage.getItem('id')
+  //   let estadoVen = false
+  //   if (tokenLocal && venLocal == 'false') {
+  //     estadoVen = true
+  //   }
+  //   const user = {id, estadoVen}
+  //   console.log(user)
+  //   const cambioVendedor = this.service.cambioVendedorServ(user).subscribe(res => {
+  //     if (cambioVendedor) {
+  //       localStorage.clear();
+  //       localStorage.setItem( "token", JSON.stringify(res));
+  //       this.logeadoVendedor = true;
+  //       this.router.navigateByUrl('/');
+  //     }
+  //   })
+  // }  
+
+
+
+
+ // const tokenLocal = localStorage.getItem('token');
+    // const venLocal = localStorage.getItem('estadoDeVendedor');
+    // // this.infoLocal = localStorage.getItem('resLog');
+    // // this.infoLocal = localStorage.getItem(JSON.parse('resLog'));
+    // // const venLocal = localStorage.getItem('estadoDeVendedor');
+    // console.log(tokenLocal)
+    // console.log(venLocal)    
+    // // console.log(this.infoLocal[1].users.dni);
+    // if (tokenLocal && venLocal == 'false') {
+    //   this.logeadoComprador = true;
+    //   this.logeadoVendedor = false;
+    // }
+    // if (tokenLocal && venLocal == 'true') {
+    //   this.logeadoVendedor = true;
+    //   this.logeadoComprador = false;
+    // }
+
+
+// let newObject = window.localStorage.getItem("resLog");
+// console.log(JSON.parse(this.newObject));
+
+// const newObject = localStorage.getItem('token');
+// const tokenLoc = localStorage.getItem(JSON.parse('resLog'))
+// console.log(tokenLoc)
+// console.log('tokenLoc2', JSON.parse(tokenLoc))
+
+/*
+if (infoLocal && !infoLocal.users.estadoDeVendedor) {
+  this.logeadoComprador = true;
+}
+if (infoLocal && infoLocal.users.estadoDeVendedor) {
+  this.logeadoVendedor = true;
+}
+*/
+  
+
+
+  
+
+
+        // localStorage.setItem( 'token', JSON.stringify(res[0].token));
+        // localStorage.setItem( 'estadoDeVendedor', JSON.stringify(res[1].users.estadoDeVendedor));
+        // localStorage.setItem( 'id', JSON.stringify(res[1].users.id));
 
     /*
     if (this.infoLocal) {
@@ -108,39 +203,22 @@ export class LoginComponent implements OnInit {
     })
     */  
   
+    
+
   
 
-  deslogear() {
-    if (this.infoLocal) {
-      if (this.logeadoVendedor || this.logeadoComprador) {
-        this.logeadoVendedor = false;
-        this.logeadoComprador = false;
-      }
-      localStorage.clear()
-      this.router.navigateByUrl('/'); 
-      this.router.navigate(['/']);
-    }
-  }
+    // if (this.infoLocal) {
+    //   if (this.logeadoVendedor || this.logeadoComprador) {
+    //     this.logeadoVendedor = false;
+    //     this.logeadoComprador = false;
+    //   }
+    //   localStorage.clear()
+    //   this.router.navigateByUrl('/'); 
+    //   this.router.navigate(['/']);
+    // }
 
-  botonVendedor() {
-    const tokenLocal = localStorage.getItem('token');
-    const venLocal = localStorage.getItem('estadoDeVendedor');
-    const id = localStorage.getItem('id')
-    let estadoVen = false
-    if (tokenLocal && venLocal == 'false') {
-      estadoVen = true
-    }
-    const user = {id, estadoVen}
-    console.log(user)
-    const cambioVendedor = this.service.cambioVendedorServ(user).subscribe(res => {
-      if (cambioVendedor) {
-        localStorage.clear();
-        localStorage.setItem( "token", JSON.stringify(res));
-        this.logeadoVendedor = true;
-        this.router.navigateByUrl('/');
-      }
-    })
-  }   
+  
+
 }
     /*
     if (this.infoLocal && !this.infoLocal.users.estadoDeVendedor) { //esto es un error ya que users no está declarado, es un resultado deuna petición 
