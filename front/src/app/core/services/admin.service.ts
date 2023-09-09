336e0b618f298202adf28788a10c2ccb6253ca42
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-09 11:26:10 -0300

    Se creó el observable credencialesAdmin, también se creó su get y su set

--- front/src/app/core/services/admin.service.ts
+++ front/src/app/core/services/admin.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../http/api.service';
 import { Credencial } from '../interfaces/credencial';
 import { DatosLogin } from '../interfaces/datosLogin';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +11,35 @@ export class AdminService {
 
   constructor(private apiService:ApiService) { 
 
+    this.credencialesAdmin = new BehaviorSubject< Credencial | null >(JSON.parse(localStorage.getItem('credencialesAdmin')!));
   }
+
+  credencialesAdmin:BehaviorSubject<Credencial | null>;
+
+  get getCredencialesAdmin(){
+    return this.credencialesAdmin.asObservable();
+  }
+
+  set setCredencialesAdmin(value:Credencial|null){
+    this.credencialesAdmin.next(value);
+  }
+
 
   iniciarSesion(datosLogin:DatosLogin):Observable<Credencial>{
 
-    return this.apiService.post('/administrator/login', datosLogin);
+    return this.apiService.post<Credencial>('/administrator/login', datosLogin).pipe(
+      map((res)=>{
+
+        const credenciales:Credencial={
+          token:res.token,
+          tipoUsuario:'admin'
+        }
+        
+        localStorage.setItem('credencialesAdmin',JSON.stringify(credenciales));
+
+        this.credencialesAdmin.next(credenciales);
+        return res;
+      })
+      );
   }
 }
