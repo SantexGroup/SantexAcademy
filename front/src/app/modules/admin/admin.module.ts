78832e54d2a5dd3c930ad9216fa0889e382e3a83
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-09 20:05:05 -0300

    Se creó el componente Tarea, se configuró su ruteo y se agrego su ruta al menú

--- front/src/app/modules/admin/admin.module.ts
+++ front/src/app/modules/admin/admin.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { TareasComponent } from './tareas/tareas.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    TareasComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ]
 })
 export class AdminModule { }
