26d53615ea3af4f77358da0d85ab5f92bde04976
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-09 11:57:22 -0300

    Se configuró la ruta de componente admin

--- front/src/app/modules/admin/admin-routing.module.ts
+++ front/src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
