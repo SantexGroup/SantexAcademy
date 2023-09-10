7fca7abd4f80aaf6d959585948f6627b0d742124
ronymari <rony.s.mari@hotmail.com>
2023-09-09 21:10:04 -0300

    error 404, nuevo nav, cards de presentacion cards de cursos, funcionalidades al footer

--- front/src/app/modules/pages/quienessomos/quienessomos.component.ts
+++ front/src/app/modules/pages/quienessomos/quienessomos.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quienessomos.component.css']
 })
 export class QuienessomosComponent implements OnInit {
+  teamMembers: any[] = [
+    {
+      name: 'Rony',
+      role: 'Desarrollador Web',
+      imageSrc: 'assets/images/team/1.jpg',
+      profileLink: '/quienes-somos/1'
+    },
+    {
+      name: 'Mariano',
+      role: 'Desarrolladora Web',
+      imageSrc: 'assets/images/team/2.jpg',
+      profileLink: '/quienes-somos/2'
+    },
+    {
+      name: 'Jorgelina',
+      role: 'Diseñador Gráfico',
+      imageSrc: 'assets/images/team/3.jpg',
+      profileLink: '/quienes-somos/3'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
--- front/src/app/modules/share/nav/nav.component.ts
+++ front/src/app/modules/share/nav/nav.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
 
-  constructor() { }
+  isMenuOpen = false;
+  isSmallScreen = false;
 
-  ngOnInit(): void {
+  constructor() {
+    // Detectar el tamaño de pantalla inicial
+    this.checkScreenSize();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    // Actualizar el valor de isSmallScreen cuando cambie el tamaño de la pantalla
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    // Cambia el valor de isSmallScreen según el tamaño de pantalla actual
+    this.isSmallScreen = window.innerWidth < 768; // Puedes ajustar el valor 768 según tus necesidades
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
