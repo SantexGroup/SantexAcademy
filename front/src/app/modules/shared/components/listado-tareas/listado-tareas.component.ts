b76852374dfbf9bb9bdf6a333aac50f4937d29a2
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-10 12:36:04 -0300

    Se movió el método inscribir voluntario de listado-tareas a tareas-voluntario

--- front/src/app/modules/shared/components/listado-tareas/listado-tareas.component.ts
+++ front/src/app/modules/shared/components/listado-tareas/listado-tareas.component.ts
@@ -1,27 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Tarea } from 'src/app/core/interfaces/tarea';
 import { TareaService } from 'src/app/core/services/tarea.service';
 import { DetalleTareaComponent } from '../detalle-tarea/detalle-tarea.component';
-import { VoluntarioService } from 'src/app/core/services/voluntario.service';
-import Swal from 'sweetalert2';
+import { Voluntario } from 'src/app/core/interfaces/voluntario';
 
 @Component({
   selector: 'app-listado-tareas',
   templateUrl: './listado-tareas.component.html',
   styleUrls: ['./listado-tareas.component.css']
 })
-export class ListadoTareasComponent implements OnInit {
-
-  constructor(private tareaService:TareaService, private dialog:MatDialog, private voluntarioService:VoluntarioService) { }
-
+export class ListadoTareasComponent implements OnInit, OnChanges {
+  
+  @Input() esVoluntario:boolean = false;
+  @Input() datosVoluntario: Voluntario | null = null;
+  
+  @Output() tareaInscripcion = new EventEmitter<Tarea>();
+  
+  listadoTareas:Tarea[] = [];
+  
+  constructor(private tareaService:TareaService, private dialog:MatDialog) { }
+  
   ngOnInit(): void {
     this.mostrarTareas();
   }
-  @Input() esVoluntario:boolean = false;
-   
-  listadoTareas:Tarea[] = [];
+  
+  ngOnChanges(changes:SimpleChanges):void{
+    
+    if(this.esVoluntario){
 
+      if('datosVoluntario' in changes){
+        
+        if(!changes['datosVoluntario'].firstChange){
+  
+          this.mostrarTareas();
+        }
+  
+      }
+
+    }
+  }
 
   mostrarTareas():void{
     this.tareaService.getTareas().subscribe({
@@ -38,48 +56,7 @@ export class ListadoTareasComponent implements OnInit {
   }
 
   inscribirVoluntario(tarea:Tarea):void{
-    
-    let idVoluntario:number;
-
-    this.voluntarioService.getDatosVoluntario.subscribe({
-      next:(res)=>{
-        idVoluntario = res?.id!;
-      }
-    });
-
-    if(idVoluntario!){
-
-      Swal.fire({
-        title: 'Estas seguro/a?',
-        text: "Deseas inscribirte en la tarea: "+tarea.name+"?",
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        cancelButtonText: 'Cancelar',
-        confirmButtonText: 'Si'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          this.tareaService.inscribirVoluntario(tarea.id!,idVoluntario!).subscribe({
-            next:()=>{
-              Swal.fire(
-                'Felicitaciones!',
-                `Te has inscripto a la tarea: ${tarea.name}`,
-                'success'
-              );
-              this.mostrarTareas();
-            },
-            error:()=>{
-              Swal.fire(
-                'Error',
-                'No se pudo realizar la inscripción.',
-                'error'
-              );
-            }
-            });
-          };
-          
-        })
-    };
-  }    
+    this.tareaInscripcion.emit(tarea);
+  }
+  
 }
