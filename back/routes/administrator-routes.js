064e453f674b6e873c7fcb09b8eb9c849a4e6412
alecar20 <alessolo@yahoo.com>
2023-09-10 15:51:55 -0300

    cambios en admin controller,routes,services

--- back/routes/administrator-routes.js
+++ back/routes/administrator-routes.js
@@ -3,7 +3,25 @@ const adminController = require('../controllers/administrator-controller');
 const { isAuthenticatedAdmin } = require('../middleware/authentication');
 
 const router = Express.Router();
-router.post('/login', adminController.login);
+
 router.get('/datos', isAuthenticatedAdmin, adminController.getData);
+router.get('/get-all-administrator', adminController.getAllAdministrator);
+router.get('/get-administrator-by-id/:id', adminController.getAdministratorById);
+router.post('/login', adminController.login);
+router.post('/create-user', adminController.createAdministrator);
+router.put('/edit-user/:id', isAuthenticatedAdmin, adminController.editAdministrator);
+router.put('/modify-password/:id', isAuthenticatedAdmin, adminController.modifyPasswordController);
+router.delete('/delete-user/:id', isAuthenticatedAdmin, adminController.deleteAdministrator);
+
+/* router.get('/get-all-volunteer', volunteerController.getAllVolunteer);
+router.get('/get-all-coordinator', coordinatorController.getAllCoordinators);
+
+router.get('/get-all-tareas', tareasController.getAllTareas);
+router.post('/create-tareas', tareasController.createTareas);
+router.put('/edit-tareas', tareasController.editTareas);
+router.delete('/delete-tareas', tareasController.deleteTareas);
+
+router.get('/get-all', premiosController.getPremios);
+router.post('/create-premios', premiosController.createPremios); */
 
 module.exports = router;
