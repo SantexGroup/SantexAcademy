1bfdb3be883f55f5bd2163c5133195666ab049dc
Nievas2 <angelgabrielnievas@gmail.com>
2023-09-11 16:38:44 -0300

    check

--- front/src/app/core/interfaces/courseCategory.ts
+++
@@ -1,4 +0,0 @@
-export interface CourseCategory{
-    id: number;
-    name: string;
-}
--- front/src/app/core/interfaces/schedule.ts
+++
@@ -1,8 +0,0 @@
-export interface Schedule{
-    id: number;
-    active: boolean;
-    where: string,
-    course: string,
-    day: string,
-    schedule: string,
-}
