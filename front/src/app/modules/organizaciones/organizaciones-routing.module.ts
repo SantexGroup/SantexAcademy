0a4b352d7ae65b3edbcd3d55e1ecce5f6ca74e85
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-11 10:19:15 -0300

    Modificación de titulo y ruta de tarea a mis-tareas

--- front/src/app/modules/organizaciones/organizaciones-routing.module.ts
+++ front/src/app/modules/organizaciones/organizaciones-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
       component:DatosOrganizacionComponent
     },
     {
-      path:'tareas',
+      path:'mis-tareas',
       component:TareasOrganizacionComponent
     }
   ]
