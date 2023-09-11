df12c63c66861e896720ae2f721a964d7156d865
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-11 10:27:26 -0300

    Se creó el componente tarea para mostrar todas las tareas

---
+++ front/src/app/modules/organizaciones/tareas/tareas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
