2ff0b0afca0da499f4224b6587a82df7ac55c44f
erickechega17 <erickechegaray10@gmail.com>
2023-09-13 17:51:29 -0300

    se añadio columna estado a tabla productos

--- back/migrations/20230913192450-createColumn-estado-products.js
+++ back/migrations/20230913192450-createColumn-estado-products.js
@@ -3,12 +3,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('Products', 'estado', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    });
   },
 
   async down (queryInterface, Sequelize) {
