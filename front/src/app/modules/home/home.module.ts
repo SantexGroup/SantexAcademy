9991cb0a617ef5b8cd8f92946df8b3f437d2db7f
FededelRincon <delrincon@gmail.com>
2023-09-13 17:16:47 -0300

    pagina de home funcional... funciona el guard

--- front/src/app/modules/home/home.module.ts
+++ front/src/app/modules/home/home.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
