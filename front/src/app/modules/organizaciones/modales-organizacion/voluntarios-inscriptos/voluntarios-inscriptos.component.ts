2303d6aadf931cef617878b12576e0c95c2895b7
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-13 20:30:18 -0300

    Creación componente voluntarios-inscriptos

---
+++ front/src/app/modules/organizaciones/modales-organizacion/voluntarios-inscriptos/voluntarios-inscriptos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voluntarios-inscriptos',
+  templateUrl: './voluntarios-inscriptos.component.html',
+  styleUrls: ['./voluntarios-inscriptos.component.css']
+})
+export class VoluntariosInscriptosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
