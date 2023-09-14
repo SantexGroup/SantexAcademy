49aea0e564b730052b34a103e42bc8540ef1d732
MauriDemasi <mauridemasi88@gmail.com>
2023-09-14 17:08:52 -0300

    Test Fork

--- back/middleware/authMiddleware.js
+++ back/middleware/authMiddleware.js
@@ -1,66 +1,66 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const  {Usuario} = require('../models');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+const { Usuario } = require("../models");
 
 const config = process.env.SESSION_SECRET;
 
-const authenticateJWT = (req, res, next) => {
+//Middleware para autenticar el usuario con rol admin
 
-    const token =
-      req.body.token || req.query.token || req.headers['x-access-token'];
+const isAdmin = async (req, res, next) => {
+  try {
+    // Asegúrate de que req.userId contenga el ID del usuario (puedes usar el middleware verifyToken para esto).
 
-    if (!token) {
-      return res.status(403).send('A token is required for authentication');
+    // Obtén el usuario desde la base de datos usando el ID
+    console.log("req.userId:", req.userId); 
+    const user = await Usuario.findByPk(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
 
-    try {
-      const decoded = jwt.verify(token, config);
-      req.user = decoded;
-    } catch (error) {
-      return res.status(401).send('Invalid Token');
-    }
-    return next();
-  }
-
-
-  const verifyToken = async (req, res, next) => {
-    try {
-      const token = req.headers["x-access-token"];
-  
-      if (!token)
-        return res.status(403).json({ message: "Token not provided" });
-  
-      const decoded = jwt.verify(token, process.env.SESSION_SECRET);
-  
-      const userId = decoded.userId; // Obtener el ID del usuario desde el token
-  
-      // Verificar si el ID del usuario en el token coincide con el ID en la URL
-      if (userId !== Number(req.params.id)) {
-        return res.status(403).json({ message: "No tienes permiso para modificar este usuario" });
-      }
-  
-      // Asignar el ID del usuario a req.userId para que esté disponible en los controladores
-      req.userId = userId;
-  
-      // Continuar con la siguiente función en la cadena de middleware
+    // Verifica si el usuario tiene el rol de administrador
+    if (user.rolesId === 2) {
+      // El usuario tiene el rol de administrador, permite la acción
       next();
-    } catch (error) {
-      console.error("Error en verifyToken:", error);
-      return res.status(401).json({ message: "Unauthorized" });
+    } else {
+      // El usuario no tiene el rol de administrador, devuelve un error
+      return res.status(403).json({ message: "No tienes permiso de administrador" });
     }
-  };
-  
-  module.exports = verifyToken;
-  
+  } catch (error) {
+    console.error("Error en isAdmin:", error);
+    return res.status(500).json({ message: "Error interno del servidor" });
+  }
+};
 
 
-  
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"];
 
-module.exports = { authenticateJWT, verifyToken  }
+    if (!token) return res.status(403).json({ message: "Token not provided" });
+
+    const decoded = jwt.verify(token, config);
+
+    const userId = decoded.userId; // Obtener el ID del usuario desde el token
+
+    // Verificar si el ID del usuario en el token coincide con el ID en la URL
+    if (userId !== Number(req.params.id)) {
+      return res
+        .status(403)
+        .json({ message: "No tienes permiso para modificar este usuario" });
+    }
+
+    // Asignar el ID del usuario a req.userId para que esté disponible en los controladores
+    req.userId = userId;
+
+    // Continuar con la siguiente función en la cadena de middleware
+    next();
+  } catch (error) {
+    console.error("Error en verifyToken:", error);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
 
-
-
-
-
+module.exports = { isAdmin, verifyToken };
