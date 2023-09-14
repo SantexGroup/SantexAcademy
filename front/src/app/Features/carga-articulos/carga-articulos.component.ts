fe2996a2feef904f47f7636b4ca20b4ddb40edd6
Mara Delgadillo <84547390+MaraD98@users.noreply.github.com>
2023-09-14 08:05:44 -0300

    Creacion de ruteo, html,css y ts de carga-articulo

--- front/src/app/Features/carga-articulos/carga-articulos.component.ts
+++ front/src/app/Features/carga-articulos/carga-articulos.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CargaArticulosComponent implements OnInit {
 
+  uploadedImages: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFileSelected(event: any): void {
+    
+    //dejar de visualizar las imagenes cuando suba otra
+    this.uploadedImages = [];
+    const files = event.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.type.startsWith('image/')) {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+
+          reader.onload = () => {
+            this.uploadedImages.push(reader.result as string);
+          };
+        }
+      }
+    }
+  }
 }
