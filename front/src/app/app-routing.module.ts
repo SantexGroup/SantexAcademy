57952ab6bd9268147433884b0981daed081ccbf3
Sebastian Ledesma <sevaledesma@gmail.com>
2023-09-14 19:00:40 -0300

    Reparando conflictos

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -13,13 +13,13 @@ import { AuthGuard } from './guards/auth/auth.guard';
  * /contact   formulario de contacto y datos de contacto de la academia
  * /dashboard para que el admin vea dashboard (aca hay que ver si estan los endpoints de TODO para mostrar en tiempo real cosas como cantidad de: alumnos, cursos, profes, "precios", "ganancias?")
  * redirect   supongo que angular tendra algo para si esta logeado mande a "/home", y si no esta logueado mande a "/"
- * 
+ 
  */
 
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
-  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
+  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
   { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule) },
   { path: 'news', loadChildren: () => import('./modules/news/news.module').then(m => m.NewsModule) },
   { path: 'contact', loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule) },
