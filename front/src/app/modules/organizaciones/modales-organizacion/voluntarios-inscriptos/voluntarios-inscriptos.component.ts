05af104466f1dfd63b6814d6d710a9b9a5aa7d6d
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-13 22:09:02 -0300

    Se desarrollo la vista y la lógica para mostrar los voluntarios inscriptos

--- front/src/app/modules/organizaciones/modales-organizacion/voluntarios-inscriptos/voluntarios-inscriptos.component.ts
+++ front/src/app/modules/organizaciones/modales-organizacion/voluntarios-inscriptos/voluntarios-inscriptos.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Tarea } from 'src/app/core/interfaces/tarea';
+import { Voluntario } from 'src/app/core/interfaces/voluntario';
+import { TareaService } from 'src/app/core/services/tarea.service';
 
 @Component({
   selector: 'app-voluntarios-inscriptos',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoluntariosInscriptosComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA)public tarea:Tarea, private tareaService:TareaService) {
 
-  ngOnInit(): void {
+
+
+  
   }
+
+  listVoluntarios:Voluntario[] = [];
+  ngOnInit(): void {
+
+    this.getVoluntariosInscriptos();
+
+  }
+
+
+  getVoluntariosInscriptos(){
+    this.tareaService.getInscriptos(this.tarea.id!).subscribe({
+      next:(res)=>{
+        this.listVoluntarios = res;
+      }
+    });
+  }
+
+
 
 }
