948767bb38c9003273da60e42def0cedab6ef8dd
CaroNew <125692041+CaroNew@users.noreply.github.com>
2023-09-17 16:50:07 -0300

    add img and description at table courses

--- back/migrations/20230825011104-create-course.js
+++ back/migrations/20230825011104-create-course.js
@@ -12,6 +12,12 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
+      description: {
+        type: Sequelize.STRING
+      }, 
+      imagen: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
