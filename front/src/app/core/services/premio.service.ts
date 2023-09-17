3bc45bc53889c91898ebc3207ae4a371371c23b2
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-16 22:16:22 -0300

    Se crearon los métodos para mostrar, crear y modificar una tarea

--- front/src/app/core/services/premio.service.ts
+++ front/src/app/core/services/premio.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../http/api.service';
+import { Observable } from 'rxjs';
+import { Premio } from '../interfaces/premio';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PremioService {
 
-  constructor() { }
+  constructor(private apiService:ApiService) { }
+
+
+  mostrar():Observable<Premio[]>{
+
+    return this.apiService.get('/premios/get-all');
+  }
+
+  crear(nuevoPremio:Premio):Observable<Premio>{
+    
+    return this.apiService.post('/premios/create-premios', nuevoPremio);
+  }
+
+  modificar(idPremio:number, premioModificado:Premio):Observable<Premio>{
+
+    return this.apiService.put('/premios/edit-premios/'+idPremio,premioModificado);
+  }
+
+
+
 }
