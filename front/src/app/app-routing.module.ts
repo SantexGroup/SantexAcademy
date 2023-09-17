c01de910d250e630247f9b3023cdae5b5f4eb451
Ludmimar <ludmilamartos@gmail.com>
2023-09-16 21:00:00 -0300

    feat(frontend) Delete unnecessary code

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
+
 const routes: Routes = [
   {
     path: 'home',
