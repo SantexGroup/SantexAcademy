a232052544f3fe02f2d277de62d9c0dca325fb10
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-09-16 22:26:29 -0300

    Se agregó la función para crear un premio y ver los premios disponibles en componente premio

--- front/src/app/modules/admin/premios/premios.component.ts
+++ front/src/app/modules/admin/premios/premios.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CrearModificarPremioComponent } from '../modales/crear-modificar-premio/crear-modificar-premio.component';
+import { ListadoPremiosComponent } from '../../shared/components/listado-premios/listado-premios.component';
 
 @Component({
   selector: 'app-premios',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PremiosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modal:MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(ListadoPremiosComponent)listadoPremios!:ListadoPremiosComponent;
+
+
+  crear():void{
+    this.modal.open(CrearModificarPremioComponent).afterClosed().subscribe({
+      next:(res)=>{
+        if(res===true) {
+          this.listadoPremios.mostrarPremios();
+        } 
+
+      }
+    });
   }
 
 }
