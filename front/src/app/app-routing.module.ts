2fe9c7ab6a9e36cdbe374368f0ceb0fd8f307d7d
Ludmimar <ludmilamartos@gmail.com>
2023-09-16 21:45:35 -0300

    fix(frontend)remove unnecessary code

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
-
 const routes: Routes = [
   {
     path: 'home',
