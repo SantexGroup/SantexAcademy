35c6603b0d94c6334fd5c16b6064d57975c15d23
SofiaBritps <sofiibritos01@gmail.com>
2023-09-18 16:40:36 -0300

    arreglando errores

---
+++ back/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+// endpoints
+
+// createUser
+// router.get('/getUser/:id', userController.getUserById)
+router.post('/createUser', userController.createUser);
+
+module.exports = router;
