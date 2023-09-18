27313c5085725a5385f0f6289890d94cd754560d
ismael <ismaelreta.programacion@gmail.com>
2023-09-18 16:18:22 -0300

    now hashed passwords work for login

--- back/routes/authenticationRouter.js
+++ back/routes/authenticationRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const authmw = require('../middleware/authentication.middleware');
 const userProvider = require('../providers/userProvider');
 
@@ -7,16 +8,20 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
+  const userPassword = await userProvider.getUserByEmail(email).password;
+  const match = await bcrypt.compare(password, userPassword);
 
-  const userloged = await userProvider.validateUser(email, password);
+  if (match) {
+    const user = await userProvider.validateUser(email, password);
 
-  if (userloged) {
-    const token = jwt.sign({ id: userloged.id, email, isAdmin: userloged.admin },
-      authmw.SECRET,
-      { expiresIn: '1h' });
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: 'Autentication failed' });
+    if (user) {
+      const token = jwt.sign({ id: user.id, email, isAdmin: user.admin },
+        authmw.SECRET,
+        { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Autentication failed' });
+    }
   }
 });
 
