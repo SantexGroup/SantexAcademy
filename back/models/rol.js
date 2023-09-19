9989e596d8bc486a443364baf98e4026438c85e2
SilJavJor <silvio.jorda@gmail.com>
2023-09-18 23:30:17 -0300

    Creacion de modulos de usuario y rol

---
+++ back/models/rol.js
@@ -0,0 +1,47 @@
+// Creacion del modelo de rol
+
+const { Model } = require('sequelize');
+//const ModeloBase = require('./modeloBase');
+
+module.exports = (sequelize, DataTypes) => {
+  //class Roles extends ModeloBase {
+  class Rol extends Model {
+      /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+
+      static associate(models) {
+      // define association here
+    }
+  }
+
+  // Inicializar la clase base (ModeloBase)
+  //super.initModeloBase(sequelize);
+
+  Rol.init({
+    id:  DataTypes.INTEGER,
+
+    descripcion: DataTypes.STRING,
+
+    activo: DataTypes.INTEGER,
+
+    por_defecto: DataTypes.INTEGER,
+
+    orden: DataTypes.INTEGER,
+
+    creado_por:  DataTypes.STRING,
+
+    fecha_creacion: DataTypes.DATE,
+
+    modificado_por:  DataTypes.STRING,
+
+    fecha_modificacion: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Rol',
+  });
+
+  return Rol;
+};
