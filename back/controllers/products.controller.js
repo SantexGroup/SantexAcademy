72a1e3a42dd9d0fe3a4eb3a5776d4375cebaed65
koritacrah <juanmcoro2003@gmail.com>
2023-09-20 10:01:40 -0300

    Update products.controller.js
    actualizacion para poder recibir muchas categorias :+1:

--- back/controllers/products.controller.js
+++ back/controllers/products.controller.js
@@ -1,5 +1,5 @@
 const productscontroller = {};
-const { Products, Categories } = require('../models') //--> nombre con el que se creo el modelo en primer lugar
+const { Products, Categories } = require("../models"); //--> nombre con el que se creo el modelo en primer lugar
 
 /**
  * @method POST
@@ -11,55 +11,59 @@ const { Products, Categories } = require('../models') //--> nombre con el que se
  * "price":number
  * "image":string
  * "status":string
- * "category": number
+ * "categories": [number] ids de las categorias a las que pertenece
  * }
  * @description metodo para crear un producto y almacenarlo en la base de datos
  */
-productscontroller.create =  async (req, res) => {
-    try {
-      let cat = null;
-      if (req.body.category) {
-         cat = await Categories.findByPk(req.body.category);
-        if (!cat) {
-          return res.status(404).json({ message: 'Category not found' });
+productscontroller.create = async (req, res) => {
+  try {
+    let cat = [];
+    const product = await Products.create(req.body);
+    console.log("categorias"+ req.body.categories  )
+    const categorias = req.body.categories
+    //recorremos la categorias y las agregamos al producto
+    if (categorias) {
+      console.log("holi")
+      for (let i = 0; i < categorias.length; i++) {
+        c = await Categories.findByPk(categorias[i]);
+        if (!c) {
+          return res.status(404).json({ message: "Producto creado, pero la categoria con id "+ categorias[i] +" no fue encontrada" });
         }
+         product.addCategories(c);
       }
-      
-      console.log("body",req.body)
-      const product = await Products.create(req.body);
-      await product.addCategories(cat);
-      res.status(201).json("Product correctly created");
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-}
+    }
+    res.status(201).json("Product correctly created");
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 /**
  * @method GET
  * @name getAll
  * @description metodo para cobtener todos los productos de la base de datos
  */
-productscontroller.getAll =  async (req, res) => {
+productscontroller.getAll = async (req, res) => {
   try {
     const productos = await Products.findAll();
     res.status(201).json(productos);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-}
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 /**
  * @method GET
  * @name getByID
  * @param {id}
  * @description metodo para cobtener todos los productos de la base de datos
  */
-productscontroller.getByID =  async (req, res) => {
+productscontroller.getByID = async (req, res) => {
   try {
     const productos = await Products.findByPk(req.params.id);
     res.status(201).json(productos);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-}
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 /**
  * @method PUT
@@ -68,24 +72,25 @@ productscontroller.getByID =  async (req, res) => {
  * @body {}
  * @description metodo para editar un producto de la base de datos
  */
-productscontroller.editByID =  async (req, res) => {
+productscontroller.editByID = async (req, res) => {
   try {
-    console.log("id",req.params.id)
-    console.log("producto que llega:",req.body);
+    console.log("id", req.params.id);
+    console.log("producto que llega:", req.body);
     const producto = await Products.findByPk(req.params.id);
-    console.log("producto encontrado",producto)
-    if (!producto) {//si no lo encontramos informamos que no existe
-      console.log("no encontro")
-      return res.status(404).json({ message: 'Product not found' });
+    console.log("producto encontrado", producto);
+    if (!producto) {
+      //si no lo encontramos informamos que no existe
+      console.log("no encontro");
+      return res.status(404).json({ message: "Product not found" });
     }
     //solucion brusca, preguntar como mandar mejor del front
     await Products.update(req.body, { where: { id: req.params.id } });
-    res.status(201).json({msg:"Product correctly updated", status: 1});
-    } catch (error) {
-      Console.log("error",error);
-      res.status(400).json({ error: error.message });
-    }
-}
+    res.status(201).json({ msg: "Product correctly updated", status: 1 });
+  } catch (error) {
+    Console.log("error", error);
+    res.status(400).json({ error: error.message });
+  }
+};
 /**
  * @method DELETE
  * @name delete
@@ -93,20 +98,19 @@ productscontroller.editByID =  async (req, res) => {
  * @param {id}
  * @description metodo para elimiar un producto almacenado en la base de datos
  */
- productscontroller.delete =  async (req, res) => { 
+productscontroller.delete = async (req, res) => {
   try {
     //buscamos el produto
     const producto = await Products.findByPk(req.params.id);
-    if (!producto) {//si no lo encontramos informamos que no existe
-      return res.status(404).json({ message: 'Product not found' });
+    if (!producto) {
+      //si no lo encontramos informamos que no existe
+      return res.status(404).json({ message: "Product not found" });
     }
     await Products.destroy({ where: { id: req.params.id } });
     res.status(201).json("Product correctly deleted");
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-}
-
-
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = productscontroller;
