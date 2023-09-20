d3491efce9b3a1ac1b4b0a8cda7e71803c47a9d9
tadeoressio <tadeo.ressio@gmail.com>
2023-09-20 19:29:59 -0300

    Se terminó interpolación, se arregló código y se guardan opciones en localStorage. Queda pendiente tomar id de producto desde pantalla anterior

--- front/src/app/Features/vista-articulo-ind/vista-articulo-ind.component.ts
+++ front/src/app/Features/vista-articulo-ind/vista-articulo-ind.component.ts
@@ -10,42 +10,133 @@ import { Router } from '@angular/router';
 export class VistaArtIndComponent implements OnInit {
 
   id: number = 1;
+  idUsuario: number = 0;
+  
+  envSel: string = '';
+  env: string = '';
+  envBol: boolean = false;
+
+  diasSel: string = '';
+  dias: string = '';
+
+  pagoSel: string = '';
+  pago: string = '';
+  pagoBol: boolean = false;
+
   respuesta: any = [];
   diasElec: string = '';
+  logeadoComprador: boolean = false;
+  logeadoVendedor: boolean = false;
+  logeadoVendedorProd: boolean = false;
+  
   
 
   constructor(private service: vistaArtIndServ, private router: Router) { }
 
   ngOnInit(): void {
-    console.log(this.id);
     this.datosProd(this.id);
+    this.corroborarLogeo();
+    this.corroborarVendedor();
+    
   }
 
   //traer datos
   datosProd(id: number) {
     this.service.datosProdServ(this.id).subscribe(res => {
       this.respuesta = res;
-      console.log(this.respuesta.usuario)
-      console.log(this.respuesta.articulos)
-      console.log(this.respuesta.tipo)
     })
+  }
+
+  //corroborar Logueo
+  corroborarLogeo() {    
+    let infoLocal = localStorage.getItem('resLog')
+    if (infoLocal) {
+      let newObject = JSON.parse(infoLocal);
+      if (newObject) {
+        if (newObject[1].users.estadoDeVendedor) {
+          this.logeadoVendedor = true;
+          this.logeadoComprador = false;
+          this.idUsuario = newObject[1].users.id;
+        } 
+        else if (!newObject[1].users.estadoDeVendedor) {
+          this.logeadoComprador = true;
+          this.logeadoVendedor = false;
+          this.idUsuario = newObject[1].users.id;
+          
+        }          
+        else {
+          this.logeadoComprador = false;
+          this.logeadoVendedor = false;
+        }
+      }
+    }
+  }
+
+  corroborarVendedor() {
+    let infoLocal = localStorage.getItem('resLog')
+    if (infoLocal) {
+      let newObject = JSON.parse(infoLocal);
+      if (newObject) {
+        if (newObject[1].users.id == this.id) {
+          this.logeadoVendedorProd = true;
+        }
+      }
+    }
   }
 
   //mensaje de alerta
   mensajeDias() {
     alert("Tanto para envío como para retiro tendrá su producto en un plazo de 2 días");
-    console.log(this.diasElec)
+    
+    
   }
+
+  capturarEnvio() {
+    this.env = this.envSel;
+    console.log(this.env)
+    if (this.env == "envio") {
+      this.envBol = true;
+    }
+  }
+
+  capturarDias() {
+    this.dias = this.diasSel;
+  }
+
+  capturarPago() {
+    this.pago = this.pagoSel;
+    if (this.pago == "tarjeta") {
+      this.pagoBol = true;
+    }
+  }
+
+  // myFunction(e) {
+  //   document.getElementById("myText").value = e.options[e.selectedIndex].getAttribute("data-adr");
+  // }
+
+  // selectChange( $event) {
+  //   //In my case $event come with a id value
+  //   this.model.myListOptions = this.listOptions[$event];
+  // }
+  // selectChange2( $event) {
+  //   //In my case $event come with a id value
+  //   ;
+  // }
 
   //confirmación
   botAlq() {
     if(confirm("¿Desea pasar a la vista de transacción?")) { 
-      
-      // localStorage.setItem( 'infoProd', JSON.stringify(
-      //   this.respuesta.usuario, 
-      //   this.respuesta.articulos, 
-      //   this.respuesta.tipo));
-      // this.router.navigateByUrl('/'); //acá iría la dirección de la vista de transacción
+      const datosAlq = {
+        idProd: this.id,
+        idAlq: this.idUsuario,
+        envio: this.envBol,
+        dias: (Number(this.dias)) + 1,
+        pago: this.pagoBol,
+      }
+      localStorage.setItem('datosAlq', JSON.stringify(datosAlq));
     }
   }  
+  redirigir() {
+    //redireccionar a registro
+  }
 }
