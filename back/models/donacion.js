bf988271a332169bfe4dd08d1a7c3d9568923578
SilJavJor <silvio.jorda@gmail.com>
2023-09-22 02:02:10 -0300

    Actualizacion Donacion y DonacionDispositivo

--- back/models/donacion.js
+++ back/models/donacion.js
@@ -1,80 +1,80 @@
-// // Creacion del modelo de donecion
+// Creacion del modelo de donecion
 
-// const { Model } = require("sequelize");
-// //const ModeloBase = require('./modeloBase');
-// const Donante = require("./donante");
-// const EstadoOperacion = require("./estadoOperacion");
+const { Model } = require("sequelize");
+//const ModeloBase = require('./modeloBase');
+const Donante = require("./donante");
+const EstadoOperacion = require("./estadoOperacion");
 
-// module.exports = (sequelize, DataTypes, Deferrable) => {
-//   //  class Usuario extends ModeloBase {
-//   class Donacion extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
+module.exports = (sequelize, DataTypes, Deferrable) => {
+  //  class Usuario extends ModeloBase {
+  class Donacion extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
 
-//     static associate(models) {
-//       // define association here
-//       Donante.belongsTo(models.Donante, { foreignKey: "id_donante" });
+    static associate(models) {
+      // define association here
+      Donante.belongsTo(models.Donante, { foreignKey: "id_donante" });
 
-//       Donante.belongsTo(models.EstadoOperacion, {
-//         foreignKey: "id_estado_operacion",
-//       });
-//     }
-//   }
+      Donante.belongsTo(models.EstadoOperacion, {
+        foreignKey: "id_estado_operacion",
+      });
+    }
+  }
 
-//   // Inicializar la clase base
-//   //super.initModeloBase(sequelize);
+  // Inicializar la clase base
+  //super.initModeloBase(sequelize);
 
-//   Donacion.init(
-//     {
-//       // id:  DataTypes.INTEGER,
-//       id: {
-//         primaryKey: true,
-//         type: DataTypes.INTEGER,
-//       },
+  Donacion.init(
+    {
+      // id:  DataTypes.INTEGER,
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
 
-//       id_doncacion: DataTypes.INTEGER,
+      id_doncacion: DataTypes.INTEGER,
 
-//       razon_social_donante: DataTypes.STRING,
+      razon_social_donante: DataTypes.STRING,
 
-//       descripcion_donacion: DataTypes.STRING,
+      descripcion_donacion: DataTypes.STRING,
 
-//       fecha_donacion: DataTypes.DATE,
+      fecha_donacion: DataTypes.DATE,
 
-//       id: DataTypes.INTEGER,
+      id: DataTypes.INTEGER,
 
-//       id_estado_operacion: {
-//         type: DataTypes.INTEGER,
+      id_estado_operacion: {
+        type: DataTypes.INTEGER,
 
-//         references: {
-//           // Se toma de la ayuda de Sequelize
-//           // This is a reference to another model
-//           model: EstadoOperacion,
+        references: {
+          // Se toma de la ayuda de Sequelize
+          // This is a reference to another model
+          model: EstadoOperacion,
 
-//           // This is the column name of the referenced model
-//           key: "id",
-//         },
-//       },
+          // This is the column name of the referenced model
+          key: "id",
+        },
+      },
 
-//       estado_operacion: DataTypes.STRING,
+      estado_operacion: DataTypes.STRING,
 
-//       activo: DataTypes.INTEGER,
+      activo: DataTypes.INTEGER,
 
-//       creado_por: DataTypes.STRING,
+      creado_por: DataTypes.STRING,
 
-//       fecha_creacion: DataTypes.DATE,
+      fecha_creacion: DataTypes.DATE,
 
-//       modificado_por: DataTypes.STRING,
+      modificado_por: DataTypes.STRING,
 
-//       fecha_modificacion: DataTypes.DATE,
-//     },
-//     {
-//       sequelize,
-//       modelName: "Donacion",
-//     }
-//   );
+      fecha_modificacion: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "Donacion",
+    }
+  );
 
-//   return Donacion;
-// };
+  return Donacion;
+};
--- back/models/donacionDispositivo.js
+++ back/models/donacionDispositivo.js
@@ -1,81 +1,81 @@
-// Creacion del modelo de donacion de dispositivo
+// // Creacion del modelo de donacion de dispositivo
 
-const { Model } = require("sequelize");
-//const ModeloBase = require('./modeloBase');
-const ModeloBase = require("./descripcionTelefono");
+// const { Model } = require("sequelize");
+// //const ModeloBase = require('./modeloBase');
+// const ModeloBase = require("./descripcionTelefono");
 
-module.exports = (sequelize, DataTypes, Deferrable) => {
-  //  class Usuario extends ModeloBase {
-  class Donante extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+// module.exports = (sequelize, DataTypes, Deferrable) => {
+//   //  class Usuario extends ModeloBase {
+//   class Donante extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
 
-    static associate(models) {
-      // define association here
-      Donante.belongsTo(models.descripcionTelefono, {
-        foreignKey: "id_descripcion_telefono",
-      });
-    }
-  }
+//     static associate(models) {
+//       // define association here
+//       Donante.belongsTo(models.descripcionTelefono, {
+//         foreignKey: "id_descripcion_telefono",
+//       });
+//     }
+//   }
 
-  // Inicializar la clase base
-  //super.initModeloBase(sequelize);
+//   // Inicializar la clase base
+//   //super.initModeloBase(sequelize);
 
-  Donante.init(
-    {
-      // id:  DataTypes.INTEGER,
-      id: {
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
+//   Donante.init(
+//     {
+//       // id:  DataTypes.INTEGER,
+//       id: {
+//         primaryKey: true,
+//         type: DataTypes.INTEGER,
+//       },
 
-      razon_social: DataTypes.STRING,
+//       razon_social: DataTypes.STRING,
 
-      email: DataTypes.STRING,
+//       email: DataTypes.STRING,
 
-      calle: DataTypes.STRING,
+//       calle: DataTypes.STRING,
 
-      numero: DataTypes.STRING,
+//       numero: DataTypes.STRING,
 
-      piso: DataTypes.STRING,
+//       piso: DataTypes.STRING,
 
-      departamento: DataTypes.STRING,
+//       departamento: DataTypes.STRING,
 
-      codigo_postal: DataTypes.STRING,
+//       codigo_postal: DataTypes.STRING,
 
-      id_descripcion_telefono: {
-        type: DataTypes.INTEGER,
+//       id_descripcion_telefono: {
+//         type: DataTypes.INTEGER,
 
-        references: {
-          // Se toma de la ayuda de Sequelize
-          // This is a reference to another model
-          model: descripcionTelefono,
+//         references: {
+//           // Se toma de la ayuda de Sequelize
+//           // This is a reference to another model
+//           model: descripcionTelefono,
 
-          // This is the column name of the referenced model
-          key: "id",
-        },
-      },
+//           // This is the column name of the referenced model
+//           key: "id",
+//         },
+//       },
 
-      descripcion_telefono: DataTypes.STRING,
+//       descripcion_telefono: DataTypes.STRING,
 
-      activo: DataTypes.INTEGER,
+//       activo: DataTypes.INTEGER,
 
-      creado_por: DataTypes.STRING,
+//       creado_por: DataTypes.STRING,
 
-      fecha_creacion: DataTypes.DATE,
+//       fecha_creacion: DataTypes.DATE,
 
-      modificado_por: DataTypes.STRING,
+//       modificado_por: DataTypes.STRING,
 
-      fecha_modificacion: DataTypes.DATE,
-    },
-    {
-      sequelize,
-      modelName: "Donante",
-    }
-  );
+//       fecha_modificacion: DataTypes.DATE,
+//     },
+//     {
+//       sequelize,
+//       modelName: "Donante",
+//     }
+//   );
 
-  return Donante;
-};
+//   return Donante;
+// };
