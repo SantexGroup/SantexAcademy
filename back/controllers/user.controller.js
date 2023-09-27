afdce00674a34fc9860ddfb37ec30e296546f34c
PabloAndresBustos <pabloandresbustos@gmail.com>
2023-09-27 20:41:59 -0300

    configuracion de UserServices

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -59,31 +59,28 @@ async function getUser(req, res, next) {
 // Controlador que redirige al servicio para actulizar un usuario
 async function updateUser(req, res, next) {
   const { id } = req.params;
-  const userData = req.body;
-  
-  if ( userData.email != ' ') {
-    try {
-    // Extraer el ID del usuario de los parámetros de la solicitud
-      //* El correo fue cambiado, se debe actualizar
-      const { name, lastName, bornDate, phone, email, pictureLink } = userData;
-      const user = await userService.updateUser(id, { name, lastName, bornDate, phone, email, pictureLink });
-      
-      res.status(200).send(user);
-    } catch (error) {
-      // Manejo de errores en caso de que ocurra algún problema
-        next(error);
-      } 
-  } else {
-      try {
-        //* No se cambia el correo, solo los demas campos
-        const { name, lastName, bornDate, phone, pictureLink } = userData;
-        const user = await userService.updateUser(id, { name, lastName, bornDate, phone, pictureLink });
-        
-        res.status(200).send(user);
-      } catch (error) {
-    // Manejo de errores en caso de que ocurra algún problema
-          next(error);
-      }
+  const {
+    name,
+    lastName,
+    bornDate,
+    phone,
+    email,
+    pictureLink,
+  } = req.body;
+
+  try {
+    const userUpdate = await userService.updateUser(
+      id,
+      name,
+      lastName,
+      bornDate,
+      phone,
+      email,
+      pictureLink,
+    );
+    res.status(200).send(userUpdate);
+  } catch (error) {
+    next(error);
   }
 }
 
