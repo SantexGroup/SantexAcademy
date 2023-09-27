4f25202c0608dad4c46e26d5bb6d7501a3db7a92
mariano <marianoludovicostock@gmail.com>
2023-09-27 17:49:18 -0300

    profile-two terminado con download PDF y cambio de colores

--- front/src/app/modules/profile-two/profile-two.component.ts
+++ front/src/app/modules/profile-two/profile-two.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChildren, ElementRef, QueryList, AfterViewInit } from '@angular/core';
 import { NavBarService } from 'src/app/core/services/toolServices/nav-bar.service';
 import { Profile } from 'src/app/core/interfaces/profile.interface';
 import { UserDataService } from 'src/app/core/services/toolServices/userData.service';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
-import *as QRCode  from 'qrcode-generator';
-
 
 
 @Component({
@@ -13,16 +11,59 @@ import *as QRCode  from 'qrcode-generator';
   templateUrl: './profile-two.component.html',
   styleUrls: ['./profile-two.component.css']
 })
-export class ProfileTwoComponent implements OnInit {
+export class ProfileTwoComponent implements OnInit, AfterViewInit {
   listProfile: Profile[] = [];
   profileData: any; // Variable para almacenar los datos proporcionados
-  imageDataUrl: string = '';
+  selectedColor: string = 'rgba(251, 205, 37, 0.957)';
+
+  backgroundColor: string = this.selectedColor;
+  textColor: string = this.selectedColor;
+  borderColor: string = this.selectedColor;
+  
+
+  @ViewChildren('cambiarColor', { read: ElementRef }) elementos!: QueryList<ElementRef>;
+
+  @ViewChildren('text-color', { read: ElementRef }) elementosConTexto!: QueryList<ElementRef>;
+
+  @ViewChildren('border-color', { read: ElementRef }) elementosConBorde!: QueryList<ElementRef>;
+
 
   constructor(
     public userData: UserDataService,
-    public views: NavBarService,    
+    public views: NavBarService,
+    private renderer: Renderer2    
   ) { }
+
+  ngAfterViewInit() {
+    this.applyColors();
+  }
+
+  applyColors() {
+    // Aplicar color de fondo a elementos con clase 'conteiner-img'
+    this.elementos.forEach((element) => {
+      if (element.nativeElement.classList.contains('conteiner-img')) {
+        this.renderer.setStyle(element.nativeElement, 'background-color', this.selectedColor);
+      }
+      
+      // Aplicar color de texto a elementos <h3> dentro de la clase 'titulo-card'
+      if (element.nativeElement.classList.contains('titulo-card')) {
+        const h3Elements = element.nativeElement.querySelectorAll('h3');
+        h3Elements.forEach((h3: HTMLElement) => { // Utilizamos HTMLElement aquí
+          this.renderer.setStyle(h3, 'color', this.selectedColor);
+        });
+      }
+      
+      // Cambiar el color del borde inferior de los elementos <h1> dentro de la clase 'nombre'
+      if (element.nativeElement.classList.contains('nombre')) {
+        const h1Elements = element.nativeElement.querySelectorAll('h1');
+        h1Elements.forEach((h1: HTMLElement) => {
+          this.renderer.setStyle(h1, 'border-bottom-color', this.selectedColor);
+        });
+      }
+    });
+  }
   
+
 
   ngOnInit(): void {
     this.userData.getMyOptionals();
@@ -33,11 +74,12 @@ export class ProfileTwoComponent implements OnInit {
     this.userData.getSkill();
   }
 
-  async downloadPDF() {
+   async downloadPDF() {
     // Selecciona el elemento con el ID 'profile-1' y captura su contenido
     const profileContainer: HTMLElement | null = document.getElementById('profile-two');
     
-    if (!profileContainer) {      
+    if (!profileContainer) {
+      console.error('No se encontró el elemento con el ID "profile-two".');
       return;
     }
 
@@ -63,7 +105,4 @@ export class ProfileTwoComponent implements OnInit {
       console.error('Error al generar el PDF:', error);
     }
   }
-
- 
-
 }
