963877d67d7afdd1ea4bf6e664d55fcd67e8ce36
Nievas2 <angelgabrielnievas@gmail.com>
2023-09-27 14:34:12 -0300

    changes in styles emails

--- back/providers/userProvider.js
+++ back/providers/userProvider.js
@@ -44,38 +44,61 @@ const createCode = async (email) => {
     user.code = code;
     user.save();
     const htmlBody = `
-  <html>
+    <!DOCTYPE html>
+    <html>
     <head>
-      <style>
-        
-        body {
-          font-family: Arial, sans-serif;
-          background-color: #f0f0f0;
-          text-align: center;
-        }
-        .container {
-          padding: 20px;
-          background-color: #ffffff;
-          border-radius: 10px;
-          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-        }
-        
-      </style>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: blue ;
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                text-align: center;
+            }
+    
+            .container {
+                padding: 20px;
+                background-color: rgba(255, 255, 255, 0.8); /* Fondo semi-transparente para mejorar la legibilidad */
+                border-radius: 10px;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+                margin: 50px auto; /* Centrar verticalmente en la pantalla */
+                max-width: 600px; /* Limitar el ancho del contenido */
+            }
+    
+            h1 {
+                color: #333; /* Cambia el color del título según tu preferencia */
+            }
+    
+            h4 {
+                color: red;
+            }
+    
+            .boton {
+                background-color: #007BFF; /* Cambia el color del botón según tu preferencia */
+                color: #fff;
+                padding: 10px 20px;
+                text-decoration: none;
+                border-radius: 5px;
+            }
+    
+            .boton:hover {
+                background-color: #0056b3; /* Cambia el color al pasar el mouse por encima */
+            }
+            b {
+              color: white;
+            }
+        </style>
     </head>
     <body>
-      <div class="container">
-        <div class="row">
-          <h1>Necesitamos validar que usted esta registrandose</h1>
+        <div class="container">
+            <h1>Necesitamos validar que usted está registrándose</h1>
+            <h4>Si no es usted, verifique hacer un cambio de contraseña o extremar sus medidas de seguridad.</h4>
+            <h3>Si es usted, por favor, vaya al siguiente link <a class="boton" href="http://localhost:4200/verificar-email/${user.email}/${user.code}"><b>verificar</b></a></h3>
         </div>
-        <div class="row">
-          <h4 style="color: red;">Si no es usted verifique hacer un cambio de contraseña o extremar sus medidas de seguridad</h4>
-          <h3>Si es usted por favor vaya al siguiente link  <a class="boton" href="http://localhost:4200/verificar-email/${user.email}/${user.code}"> verificar</a></h3>
-        </div>
-      </div>
     </body>
-  </html>`;
+    </html>
+    `;
 
     const transporterSe = await transporter.sendMail({
       from: process.env.EMAIL,
