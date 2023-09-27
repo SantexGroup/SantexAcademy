fe68d07d4064614afbfa58048a988248bd0b5c08
FededelRincon <delrincon@gmail.com>
2023-09-27 20:02:40 -0300

    fix options in dropdown in /home

--- front/src/app/modules/shared/components/search-nav/search-nav.component.ts
+++ front/src/app/modules/shared/components/search-nav/search-nav.component.ts
@@ -10,13 +10,12 @@ export class SearchNavComponent implements OnInit {
   constructor() { }
 
   texto: string = ''; // Variable para el input
-  opcionSeleccionada: string = 'opcion1'; // Variable para el dropdown
+  opcionSeleccionada: 'ByName' | 'byProfesor' = 'ByName'; // Variable para el dropdown
 
   ngOnInit(): void {
   }
 
   buscar() {
-    // Aquí puedes implementar la lógica de búsqueda con los valores de 'texto' y 'opcionSeleccionada'
     console.log('Texto:', this.texto);
     console.log('Opción seleccionada:', this.opcionSeleccionada);
   }
