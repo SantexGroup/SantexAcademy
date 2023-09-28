df33cb8015791d71f7b2c275d11f5e3ed4b66483
Martin Fernandez <fernandezmartin.k26@gmail.com>
2023-09-28 19:46:22 -0300

    Trabajando en el dashboard de usuario, agregando funcion para cargar foto

--- front/src/app/components/students/students/students.component.ts
+++ front/src/app/components/students/students/students.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-students',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  user = {
+    name: 'Nombre',
+    lastName: 'Apellido',
+    email: 'correo@example.com', // Reemplaza esto con el correo real del usuario
+    profileImage: '',
+  };
+
+  showImage = false; // Variable para controlar si se muestra la imagen
+
+  @ViewChild('fileInput') fileInput!: ElementRef; // Usamos '!' para indicar que será inicializada
 
   constructor() { }
 
   ngOnInit(): void {
+    // Aquí debes obtener los datos del usuario desde tu servicio o donde los tengas disponibles.
   }
 
+  handleImageUpload(event: any) {
+    const file = event.target.files[0];
+    
+    // Aquí puedes agregar código para subir la imagen al servidor si es necesario.
+    // Luego, establece showImage en true para mostrar la imagen.
+    this.showImage = true;
+  }
+
+  // Función para abrir el campo de entrada de archivo al hacer clic en "Subir foto"
+  openFileInput() {
+    this.fileInput.nativeElement.click();
+  }
 }
