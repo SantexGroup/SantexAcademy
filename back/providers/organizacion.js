3691de11e3f65687a57ae8e537e61894fe347b8b
ExequielMaydana <exedevcoding22@gmail.com>
2023-09-28 14:19:54 -0300

    upload cover_page in progress

--- back/providers/organizacion.js
+++ back/providers/organizacion.js
@@ -80,25 +80,24 @@ const getOrganizations = async () => {
   }
 };
 
-
 const getOrganizationsById = async (orgId) => {
   try {
     const organization = await Organizacion.findOne({
       where: {
         id: orgId,
-      }
-    })
+      },
+    });
 
     if (!organization) {
       throw new Error("Organization not found");
     }
 
     return organization;
-  }catch (error) {
+  } catch (error) {
     console.error("Ocurrió un error al obtener la organización.", error);
     throw error;
   }
-}
+};
 const updateOrganizationById = async (id, organization) => {
   try {
     const [affectedRows] = await Organizacion.update(organization, {
@@ -190,6 +189,8 @@ const getOrganizationByLocation = async (location, opportunityType) => {
     throw error;
   }
 };
+
+// const addCoverPage = async (id, img) => {};
 
 module.exports = {
   loginOrg,
