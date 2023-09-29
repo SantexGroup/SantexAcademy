aa982c2500e0756aaf1f53604cd553405740b1e1
melisaapaz <melisaapaz@gmail.com>
2023-09-30 00:26:03 +0200

    corrección de deslogueo desde perfil de alumno

--- front/src/app/modules/pages/perfil-alumno/perfil-alumno.component.ts
+++ front/src/app/modules/pages/perfil-alumno/perfil-alumno.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-perfil-alumno',
@@ -6,14 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil-alumno.component.css'],
 })
 export class PerfilAlumnoComponent {
+  isLoggedIn: boolean = true;
+
   perfilDelAlumno = {
     nombre: 'Nombre del Alumno',
     edad: 20,
     // Otros datos del perfil
-
-
     }
+
+    constructor(private router: Router,private authService: AuthService) {}
   
+    logout(): void {
+      this.router.navigateByUrl('/login');
+      this.authService.logout();
+      console.log("Deslogueo")
+      this.isLoggedIn = false;
+    }
   };
 
 
