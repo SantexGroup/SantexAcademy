5dbe93c4bc96087d9e06e3d899cdc6cb60109a93
Gerardo Fabian Quispe <gfq0684@gmail.com>
2023-10-03 14:38:19 -0300

    agregamos el contador de producto del carrito, para cada vez que se agrega un producto

--- front/src/app/modules/dashboard/nav-bar/nav-bar.component.ts
+++ front/src/app/modules/dashboard/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service';
 
 export interface menuItem {
   name: string,
@@ -11,16 +14,33 @@ export interface menuItem {
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  
   menu: menuItem[] = [{name: "Home", paht: "home"}, {name: "Agregar Producto", paht: "form-add-product"}, {name: "Actualizar Producto", paht:"form-update"}, {name: "Buscar Producto", paht: "search-product"}, {name: "Eliminar Producto", paht: "form-delete"}];
-
+  
   isMenuOpen: boolean = false;
   
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
+  total$: Observable<number>;
+
+  constructor(private shoppingCartService: ShoppingCartService, private elRef: ElementRef) { 
+    this.total$ = this.shoppingCartService.cart$
+      .pipe(
+        map(products => products.length)
+      );
+
   }
 
-  constructor() { }
+  @HostListener('document:click', ['$event'])
+  onDocumentClick($event: MouseEvent): void {
+    const elementoExcluido = this.elRef.nativeElement.querySelector('.header-menu-button');
+    if (!elementoExcluido.contains($event.target)) {
+      // Si no hace click en el boton de menu.
+      this.isMenuOpen = false;
+    } else {
+      // Si hace click en el boton de menu.
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+  }
+
 
   ngOnInit(): void {
   }
