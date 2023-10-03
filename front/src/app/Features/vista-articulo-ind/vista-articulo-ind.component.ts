80f768dd6134b8c5ae684decdf3a94541fd9c62e
tadeoressio <tadeo.ressio@gmail.com>
2023-10-03 17:16:41 -0300

    Arreglo de visualizar proudcto

--- front/src/app/Features/vista-articulo-ind/vista-articulo-ind.component.ts
+++ front/src/app/Features/vista-articulo-ind/vista-articulo-ind.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { vistaArtIndServ } from 'src/app/core/services/vista-art-ind-serv.service';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { BarraService } from 'src/app/core/services/barra.service';
 
 @Component({
   selector: 'app-vista-articulo-ind',
@@ -10,8 +11,10 @@ import { environment } from 'src/environments/environment';
 })
 export class VistaArtIndComponent implements OnInit {
 
-
-  
+  //categorias
+  listcategorias: any[] = [];
+  catArt: string = '';
+  cat: any = [];
   // obtener imgs
   servidor: string = environment.API_URL + '/images/';
   images: string[] = [];
@@ -20,7 +23,8 @@ export class VistaArtIndComponent implements OnInit {
   id: number = 0;
 
   //id del usuario
-  idUsuario: number = 1;
+  idUsuario: number = 0;
+  nomUSer: string = '';
 
   //Mayúsculas
   nombreArt: string = '';
@@ -29,17 +33,20 @@ export class VistaArtIndComponent implements OnInit {
   //seleccionables
   envSel: string = '';
   env: string = '';
+  envBolOri: boolean = false;
   envBol: boolean = false;
 
-  diasSel: string = '';
-  dias: string = '';
+  diasSel: number = 0;
+  dias: number = 0;
 
   pagoSel: string = '';
   pago: string = '';
+  pagoBolOri: boolean = false;
   pagoBol: boolean = false;
 
   //traer datos
   respuesta: any = [];
+  respuestaUser: any = [];
 
   //estados logueo
   logeadoComprador: boolean = false;
@@ -48,7 +55,7 @@ export class VistaArtIndComponent implements OnInit {
   
   
 
-  constructor(private service: vistaArtIndServ, private router: Router) { }
+  constructor(private service: vistaArtIndServ, private router: Router, private barraService: BarraService) { }
 
   ngOnInit(): void {
     this.getIdProd();
@@ -64,29 +71,48 @@ export class VistaArtIndComponent implements OnInit {
       this.id = newObject;
     }
   }
-
   //traer datos
   datosProd(id: number) {
+    this.barraService.getCategories().subscribe(categorias => {this.listcategorias = categorias});
     this.service.datosProdServ(id).subscribe(res => {
       this.respuesta = res;
       console.log("Respuesta: " + JSON.stringify(this.respuesta));
-      this.nombreArt = res.nombre.charAt(0).toUpperCase() + res.nombre.slice(1)
-      console.log("nombre art: " + this.nombreArt)
-      this.descArt = res.detalles.charAt(0).toUpperCase() + res.detalles.slice(1)
-      console.log("desc art: " + this.descArt)
-      console.log("cateogria: " + res.detalles)
-
-      if (res.Images){
-        const imagesProd = res.Images
-        for (let i = 0; i < imagesProd.length; i++){
-          this.images.push(this.servidor + imagesProd[i].url);
+      //reemplazar datos
+      if(res) {
+        //nombre art, desc y envio
+        this.nombreArt = JSON.stringify(res.nombre);
+        this.nombreArt = res.nombre.charAt(0).toUpperCase() + res.nombre.slice(1);
+        this.descArt = JSON.stringify(res.detalles);
+        this.descArt = res.detalles.charAt(0).toUpperCase() + res.detalles.slice(1);
+        if (res.envio == true) {
+          this.envBolOri = true;
         }
+         //buscar categoria prod y eliminar de array
+        for (let i=0; i < this.listcategorias.length; i++) {
+          if (this.respuesta.idTipoProducto == this.listcategorias[i].id) {
+            this.catArt = JSON.stringify(this.listcategorias[i].name.charAt(0).toUpperCase() + this.listcategorias[i].name.slice(1));
+            this.catArt = this.catArt.slice(1, this.catArt.length-1);
+            console.log("Categorái: " + this.catArt)
+          }
+          if (this.catArt != this.listcategorias[i].name.charAt(0).toUpperCase() + this.listcategorias[i].name.slice(1)) {
+            let newList = this.cat;
+            newList.push(this.listcategorias[i]);
+            this.cat = newList;
+          }
+        }
+        if (this.respuesta.Images){
+          const imagesProd = this.respuesta.Images
+          for (let i = 0; i < imagesProd.length; i++){
+            this.images.push(this.servidor + imagesProd[i].url);
+          }
+        }
+        this.service.datosUsuario(this.respuesta.idUsuario).subscribe(user => {
+          this.respuestaUser = user;
+          this.nomUSer = this.respuestaUser.firstName.charAt(0).toUpperCase() + this.respuestaUser.firstName.slice(1) + ' ' + (this.respuestaUser.lastName.charAt(0).toUpperCase() + this.respuestaUser.lastName.slice(1));
+        })
       }
-    })
-    console.log("imagenes: " + this.images);
+    }) 
   }
-
-
   //corroborar logueos
   corroborarLogeo() {    
     let infoLocal = localStorage.getItem('resLog')
@@ -102,7 +128,6 @@ export class VistaArtIndComponent implements OnInit {
           this.logeadoComprador = true;
           this.logeadoVendedor = false;
           this.idUsuario = newObject[1].users.id;
-          
         }          
         else {
           this.logeadoComprador = false;
@@ -111,7 +136,6 @@ export class VistaArtIndComponent implements OnInit {
       }
     }
   }
-
   corroborarVendedor() {
     let infoLocal = localStorage.getItem('resLog')
     if (infoLocal) {
@@ -123,14 +147,10 @@ export class VistaArtIndComponent implements OnInit {
       }
     }
   }
-
-
   //mensaje de alerta
   mensajeDias() {
     alert("Tanto para envío como para retiro tendrá su producto en un plazo de 2 días");    
   }
-
-
   //seleccionables
   capturarEnvio() {
     this.env = this.envSel;
@@ -139,19 +159,15 @@ export class VistaArtIndComponent implements OnInit {
       this.envBol = true;
     }
   }
-
   capturarDias() {
     this.dias = this.diasSel;
   }
-
   capturarPago() {
     this.pago = this.pagoSel;
     if (this.pago == "tarjeta") {
       this.pagoBol = true;
     }
   }
-
-  
   //confirmación
   botAlq() {
     if(confirm("¿Desea pasar a la vista de transacción?")) { 
