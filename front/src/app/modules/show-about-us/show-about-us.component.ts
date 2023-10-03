56e0fd2f38ed2c84ec3ae98dcb45168d2fd8d71f
ismael <ismaelreta.programacion@gmail.com>
2023-10-03 14:55:57 -0300

    removed debuggin console logs

--- front/src/app/modules/show-about-us/show-about-us.component.ts
+++ front/src/app/modules/show-about-us/show-about-us.component.ts
@@ -24,9 +24,6 @@ export class ShowAboutUsComponent implements OnInit {
       this.groupedAbouts[about.priority].abouts.push(about);
     });
     this.groupedAbouts = this.groupedAbouts.filter(abouts => abouts.abouts.length > 0);
-    console.log('abouts', this.abouts)
-    console.log('grouped abouts', this.groupedAbouts)
-    console.log('maxpriority', maxPriority)
   });
   }
 
