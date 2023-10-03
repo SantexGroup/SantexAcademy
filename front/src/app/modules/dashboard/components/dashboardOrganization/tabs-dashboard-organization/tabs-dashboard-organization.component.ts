92f546c1dd0c605abea61a47cf6785e7421d5ef8
ExequielMaydana <exedevcoding22@gmail.com>
2023-10-03 11:42:49 -0300

    resolve conflic dashboard org

--- front/src/app/modules/dashboard/components/dashboardOrganization/tabs-dashboard-organization/tabs-dashboard-organization.component.ts
+++ front/src/app/modules/dashboard/components/dashboardOrganization/tabs-dashboard-organization/tabs-dashboard-organization.component.ts
@@ -3,38 +3,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DashboardServicesService } from '../../../services/dashboard-services.service';
 import { Store } from '@ngrx/store';
 import { selectToken } from 'src/app/core/auth.selectors';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs-dashboard-organization',
   templateUrl: './tabs-dashboard-organization.component.html',
   styleUrls: ['./tabs-dashboard-organization.component.css'],
 })
-
-export class TabsDashboardOrganizationComponent implements OnInit {
-
+export class TabsDashboardOrganizationComponent {
   @Input() dataTabs: any = {};
   @ViewChild('fileInput') fileInput: ElementRef | undefined;
+
   userForm: FormGroup;
 
   activeTab: number = 1;
   editData: boolean = false;
+  fadeAnimationClass = '';
+  selectedCategory: string = '';
+  image: string = '';
 
-  tokenOrg: string = "";
   onModalStatus: boolean = false;
   statusModal: string = '';
   textBtnModalStatus: string = '';
   textMessage: string = '';
   onModalQuestion: boolean = false;
-  fadeAnimationClass = '';
 
-  ngOnInit(): void {
-    this.tokenOrg = this.authService.getAuthToken()
-  }
-
-  userForm: FormGroup;
-  constructor(private formBuilder: FormBuilder, private dashService: DashboardServicesService, private authService: AuthService, private router: Router) {
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private dashService: DashboardServicesService,
+    private router: Router,
+    private store: Store
+  ) {
     this.userForm = this.formBuilder.group({
       name: [''],
       cuit: [''],
@@ -46,26 +45,11 @@ export class TabsDashboardOrganizationComponent implements OnInit {
       email: ['', [Validators.email]],
       category: [''],
       description: [''],
-      // urlWebSite: [''],
+      urlWebSite: [''],
     });
   }
 
-  image: string =
-    'https://res.cloudinary.com/carina-bosio/image/upload/v1695591464/xAcademy/Asociaci%C3%B3n_Civil_Manos_Abiertas-removebg-preview_2_ltnrr4.png';
-
-  newValues: { [key: string]: string } = {
-    image: '',
-    name: '',
-    cuit: '',
-    location: '',
-    phone: '',
-    email: '',
-    category: '',
-    description: '',
-  };
-
   activeEditProfileOrg() {
-
     this.fadeAnimationClass = 'fade-in-out-animation';
 
     this.userForm.setValue({
@@ -76,7 +60,7 @@ export class TabsDashboardOrganizationComponent implements OnInit {
       email: this.dataTabs.email,
       category: this.dataTabs.category,
       description: this.dataTabs.description,
-      // urlWebSite: this.dataTabs.urlWebSite,
+      urlWebSite: this.dataTabs.urlWebSite,
     });
     this.editData = true;
   }
@@ -91,10 +75,25 @@ export class TabsDashboardOrganizationComponent implements OnInit {
               .updateProfileOrganization(userData, token)
               .subscribe({
                 next: (res) => {
-                  console.log(res);
+                  if (res) {
+                  }
+                  this.onModalStatus = true;
+                  this.statusModal = 'success';
+                  this.textMessage =
+                    ' ¡Tus datos se han modificado correctamente!';
+
+                  setTimeout(() => {
+                    this.onModalStatus = false;
+                    window.location.reload();
+                  }, 3000);
                 },
                 error: (err) => {
-                  console.log(err);
+                  console.log('error when editing the user', err);
+                  this.onModalStatus = true;
+                  this.statusModal = 'failed';
+                  this.textBtnModalStatus = 'Aceptar';
+                  this.textMessage =
+                    '¡Se produjo un error al modificar tus datos! Por favor, inténtalo de nuevo más tarde.';
                 },
               });
           }
@@ -102,63 +101,25 @@ export class TabsDashboardOrganizationComponent implements OnInit {
         });
       }
     }
-
-  }
-  captureData(fieldName: string, event: any) {
-    this.newValues[fieldName] = event.target.value;
-  }
-
-  handleImageUpload(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      this.image = URL.createObjectURL(file);
-    }
-  }
-
-
-  selectedCategory: string = '';
-
-  onCategoryChange(event: any) {
-    this.selectedCategory = event.target.value;
-    this.captureData('category', this.selectedCategory);
-
-    this.userForm.setValue({
-      category: event.target.value,
-    });
-  }
-
-  editarDatos() {
-    this.editData = false;
-    this.fadeAnimationClass = 'fade-in-out-animation';
-  }
-
-
-  handleProfileDelete() {
-    this.onModalQuestion = !this.onModalQuestion;
   }
 
   deleteProfile() {
-    this.dashService.deleteProfileOrganization(this.tokenOrg).subscribe({
-      next: (res) => {
-        if (res) {
-          this.authService.clearAuthToken();
-          this.router.navigate(['']);
-        }
-      },
-      error: (err) => {
-        console.log('error deleting user', err);
-      },
+    this.store.select(selectToken).subscribe((token) => {
+      if (token) {
+        this.dashService.deleteProfileOrganization(token).subscribe({
+          next: (res) => {
+            if (res) {
+              this.store;
+              this.router.navigate(['']);
+            }
+          },
+          error: (err) => {
+            console.log('error deleting user', err);
+          },
+        });
+      }
     });
   }
-
-  activateTab(tabNumber: number) {
-    this.activeTab = tabNumber;
-  }
-
-
-
-  @ViewChild('fileInput') fileInput: ElementRef | undefined;
-
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
@@ -169,6 +130,21 @@ export class TabsDashboardOrganizationComponent implements OnInit {
       };
       reader.readAsDataURL(file);
     }
+  }
+
+  onCategoryChange(event: any) {
+    this.selectedCategory = event.target.value;
+    this.userForm.setValue({
+      category: event.target.value,
+    });
+  }
+
+  handleProfileDelete() {
+    this.onModalQuestion = !this.onModalQuestion;
+  }
+
+  activateTab(tabNumber: number) {
+    this.activeTab = tabNumber;
   }
 
   closeModalStatus() {
