71185d69d4df58f0cf7229659b3b3dd289564d61
unknown <verojuy.gonzalez@gmail.com>
2023-10-03 23:34:33 -0300

    product-paginado

--- back/providers/Products.js
+++ back/providers/Products.js
@@ -1,55 +1,48 @@
+const { Product } = require('../models/product');
 
- const{Product}=require("../models/product")
+const getProducts = async (req, res) => {
+  try {
+    const pageAsNumber = Number.parseInt(req.query.page, 10);
+    const sizeAsNumber = Number.parseInt(req.query.size, 10);
 
-
-
-const getProducts= async(req,res)=>{
-
-    try {   
-        const pageAsNumber=Number.parseInt(req.query.page);
-        const sizeAsNumber=Number.parseInt(req.query.size);
-        
-        let page=0;
-        if(!Number.isNaN(pageAsNumber) && pageAsNumber > 0){
-            page=pageAsNumber;
-        }
-
-        let size =10;
-        if(!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber < 10){
-            size= sizeAsNumber;
-        }
-
-    
-        const allProducts= await Product.findAndCountAll({
-            limit:size,
-            offset:page * size
-        });
-       res.status(200).json(allProducts)
-    } catch (error) {
-       res.status(400).json("Error al cargar los datos")
+    let page = 0;
+    if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
+      page = pageAsNumber;
     }
-}
-const getProdByName= async(req,res)=>{
-    const name= req.query.name;
-    try {
-        if(name){
-            const existe= await Product.findOne(
-                {
-                where:{
-                    name:name,
 
-                    },
-                }
-            );
-            res.status(200).json(existe) 
-        }
-   
-     } catch (error) {
-      console.error(error); 
-      res.status(500).json({ message: 'Error al cargar los datos', error: error.message });
+    let size = 10;
+    if (!Number.isNaN(sizeAsNumber) && sizeAsNumber > 0 && sizeAsNumber < 10) {
+      size = sizeAsNumber;
     }
-} 
-module.exports={
-    getProducts,
-    getProdByName
-}
+
+    const allProducts = await Product.findAndCountAll({
+      limit: size,
+      offset: page * size,
+    });
+    res.status(200).json(allProducts);
+  } catch (error) {
+    res.status(400).json('Error al cargar los datos');
+  }
+};
+const getProdByName = async (req, res) => {
+  const { name } = req.query;
+  try {
+    if (name) {
+      const existe = await Product.findOne(
+        {
+          where: {
+            name,
+
+          },
+        },
+      );
+      res.status(200).json(existe);
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al cargar los datos', error: error.message });
+  }
+};
+module.exports = {
+  getProducts,
+  getProdByName,
+};
--- back/routes/products.route.js
+++ back/routes/products.route.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const productValidator = require('../validations/product.validator');
 const productController = require('../controllers/product.controller');
 const JWTDecoder = require('../providers/jwt_decoder');
+const { getProducts, getProdByName } = require('../providers/Products');
 
 const productRouter = Router();
 
@@ -13,7 +14,7 @@ productRouter.post(
 );
 
 productRouter.get('/products', JWTDecoder, (_req, res) => res.send('OK'));
- productRouter.get("/products",getProducts);
- productRouter.get("/", getProdByName);
+productRouter.get('/products', getProducts);
+productRouter.get('/', getProdByName);
 
 module.exports = productRouter;
