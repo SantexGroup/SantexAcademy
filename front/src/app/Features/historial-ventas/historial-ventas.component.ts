79bf4174c76a3efcac744992cc91c75a6dca3e04
tadeoressio <tadeo.ressio@gmail.com>
2023-10-04 20:02:10 -0300

    Modelo y for para datos

--- front/src/app/Features/historial-ventas/historial-ventas.component.ts
+++ front/src/app/Features/historial-ventas/historial-ventas.component.ts
@@ -8,22 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./historial-ventas.component.css']
 })
 export class HistorialVentasComponent implements OnInit {
-
-  nomArt: string = '';
-  nomVen: string = '';
-
+  
+  idVen: number = 0;
+  modelo: any = {
+    nomVen: '',
+    nomArt: '',
+    catArt: '',
+    cat: [],
+    idArt: 0,
+  }
   listcategorias: any[] = [];
-  catArt: string = '';
-  cat: any = [];
 
   desArt: string = '';
-
   envArt: string = '';
   pagArt: string = '';
   preArt: string = '';
-
   articulosVen: any = [];
-  idVen: number = 0;
   datosLog: any = {};
   respuesta: any = [];
 
@@ -40,53 +40,56 @@ export class HistorialVentasComponent implements OnInit {
       let newObject = JSON.parse(datosLog);
       this.datosLog = newObject;
       this.idVen = this.datosLog[1].users.id;
+      console.log("modelo: " + this.modelo.nomVen)
     }
-    //traer articulos, nomArt, desArt, preArt e idArt
-    this.service.articulosVendedor(this.idVen).subscribe(res => {
-      console.log("articulos: " + JSON.stringify(res));
-      this.respuesta = res;
-      this.nomArt = res[1].nombre;
-      this.desArt = res[1].detalles;
-      this.preArt = res[1].precio;
-      if(res[1].envio) {
-        this.envArt = 'envío y retiro';
-      }else {
-        this.envArt = 'Retiro'
-      }
-    })
-    //nombre categoria producto
-    this.service.getCategories().subscribe(categorias => {
-      this.listcategorias = categorias
-      console.log("cateogiras: " + JSON.stringify(this.listcategorias))
-      for (let i=0; i < this.listcategorias.length; i++) {
-        if (this.respuesta[1].idTipoProducto == this.listcategorias[i].id) {
-          this.catArt = JSON.stringify(this.listcategorias[i].name.charAt(0).toUpperCase() + this.listcategorias[i].name.slice(1));
-          this.catArt = this.catArt.slice(1, this.catArt.length-1);
-        }
-        if (this.catArt != this.listcategorias[i].name.charAt(0).toUpperCase() + this.listcategorias[i].name.slice(1)) {
-          let newList = this.cat;
-          newList.push(this.listcategorias[i]);
-          this.cat = newList;
-        }
-      }
-    });
     //nombre vendedor
     this.service.infoVendedor(this.idVen).subscribe(resVen => {
-      this.nomVen = JSON.stringify(resVen.firstName.charAt(0).toUpperCase() + resVen.firstName.slice(1) + ' ' + (resVen.lastName.charAt(0).toUpperCase() + resVen.lastName.slice(1)));
-      this.nomVen = this.nomVen.slice(1, this.nomVen.length-1);
+      this.modelo.nomVen = JSON.stringify(resVen.firstName.charAt(0).toUpperCase() + resVen.firstName.slice(1) + ' ' + (resVen.lastName.charAt(0).toUpperCase() + resVen.lastName.slice(1)));
+      this.modelo.nomVen = this.modelo.nomVen.slice(1, this.modelo.nomVen.length-1);
+    });
+    //traer categorias
+    this.service.getCategories().subscribe(categorias => {this.listcategorias = categorias, console.log("cateogrías: " + JSON.stringify(this.listcategorias))});
+    //traer articulos, nomArt, desArt, preArt e idArt
+    this.service.articulosVendedor(this.idVen).subscribe(res => {
+      this.respuesta = res;
+      console.log("articulos: " + JSON.stringify(res));
+      console.log("Cantidad articulos: " + res.length)
+      for (let i=0; i < res.length; i++) {
+        this.modelo.idArt = res[i].id;
+        this.modelo.nomArt = res[i].nombre;
+        this.desArt = res[i].detalles;
+        this.preArt = res[i].precio;
+        if(res[i].envio) {
+          this.envArt = 'envío y retiro';
+        }else {
+          this.envArt = 'retiro'
+        }  
+        //nombre categoria producto
+        for (let i=0; i < this.listcategorias.length; i++) {
+          if (this.respuesta[1].idTipoProducto == this.listcategorias[i].id) {
+            this.modelo.catArt = JSON.stringify(this.listcategorias[i].name.charAt(0).toUpperCase() + this.listcategorias[i].name.slice(1));
+            this.modelo.catArt = this.modelo.catArt.slice(1, this.modelo.catArt.length-1);
+          }
+          if (this.modelo.catArt != this.listcategorias[i].name.charAt(0).toUpperCase() + this.listcategorias[i].name.slice(1)) {
+            let newList = this.modelo.cat;
+            newList.push(this.listcategorias[i]);
+            this.modelo.cat = newList;
+          }
+        }
+      }  
     })
   }
   //redirecciones acciones
   redireccion1() {
-    localStorage.setItem('idProd', JSON.stringify(this.idVen))
+    localStorage.setItem('idProd', JSON.stringify(this.modelo.idArt));
     this.router.navigate(['vista-articulo']);
   }
   redireccion2() {
-    localStorage.setItem('idProd', JSON.stringify(this.idVen))
+    localStorage.setItem('idProd', JSON.stringify(this.modelo.idArt));
     this.router.navigate(['modificar-articulo']);    
   }
   redireccion3() {
-    localStorage.setItem('idProd', JSON.stringify(this.idVen))
+    localStorage.setItem('idProd', JSON.stringify(this.modelo.idArt));
     // this.router.navigate(['modificar-articulo']);    
   }
 }
