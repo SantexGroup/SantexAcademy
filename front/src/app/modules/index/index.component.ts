b4bcef9fec20e3a4e2ef94ebc78f4b396848ccc5
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-03 23:36:30 -0300

    Se unificó el login

--- front/src/app/modules/index/index.component.ts
+++ front/src/app/modules/index/index.component.ts
@@ -3,9 +3,7 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Credencial } from 'src/app/core/interfaces/credencial';
-import { AdminService } from 'src/app/core/services/admin.service';
-import { OrganizacionService } from 'src/app/core/services/organizacion.service';
-import { VoluntarioService } from 'src/app/core/services/voluntario.service';
+import { CuentaService } from 'src/app/core/services/cuenta.service';
 
 @Component({
   selector: 'app-index',
@@ -17,34 +15,24 @@ export class IndexComponent implements OnInit {
 
   
   @ViewChild('sideNav') sideNav!:MatSidenav;
-  constructor(private router:Router, organizacionService:OrganizacionService,voluntarioService:VoluntarioService, adminService:AdminService) {
+  constructor(private router:Router, cuentaService:CuentaService) {
 
-    this.credencialesOrganizacion$ = organizacionService.getCredencialesOrganizacion;
 
-    this.credencialesVoluntario$ = voluntarioService.getCredencialesVoluntario;
-
-    this.credencialesAdmin$ = adminService.getCredencialesAdmin;
-
+    this.credencialesUsuario$ = cuentaService.getCredencialesUsuario;
 
   }
   
-  credencialesOrganizacion$:Observable<Credencial|null>;
-  credencialesVoluntario$:Observable<Credencial|null>;
-  credencialesAdmin$:Observable<Credencial| null>;
+  credencialesUsuario$:Observable<Credencial| null>;
 
   ngOnInit(): void {
   }
 
-  irLoginVoluntarios(){
-    this.router.navigate(['/index/login'],{queryParams:{tipo:'voluntario'}});
+  irLogin(){
+    this.router.navigate(['/index/login']);
     this.sideNav.close();
   }
 
-  irLoginOrganizaciones(){
-    this.router.navigate(['/index/login'],{queryParams:{tipo:'organizacion'}});
-    this.sideNav.close();
-  }
-
+  
  redireccionarA(ruta:string):void{
     this.router.navigate([ruta]);
     this.sideNav.close();
