9158210f082a7f91a516623edae79f661b0cc86e
MauriDemasi <mauridemasi88@gmail.com>
2023-10-05 17:01:29 -0300

    Recovery Pass in Progress

--- back/config/db-config.js
+++ back/config/db-config.js
@@ -2,12 +2,12 @@ const { Sequelize } = require("sequelize"); // Take the Sequelize Class
 require("dotenv").config();
 
 // Se crea la instancia de sequelize con los datos de conexión a la base de datos en el CONSTRUCTOR
-const sequelize = new Sequelize();
+const sequelize = new Sequelize(
 // process.env.DB_DATABASE,
 // process.env.DB_USERNAME,
 // process.env.DB_PASSWORD,
-// process.env.DB_URL
-
+ process.env.DB_URL_LOCAL,
+);
 //luego se pasa un objeto de configuración con el host y el dialecto
 // {
 //   host: process.env.DB_HOST,
