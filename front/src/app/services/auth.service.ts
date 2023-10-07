6cd17ca79aa2cbaef26a903a3ed493ca1f4bacd0
FededelRincon <delrincon@gmail.com>
2023-10-07 15:02:45 -0300

    auth modificado, se agrego isAdministrador esta pendiente de conexion con la respuesta del backend

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -9,23 +9,40 @@ import { baseURL } from '../../../src/config';
 export class AuthService {
 
   private isAuthenticated = false;
-  // para deshabilitar el guard pasar esto a true
+  private isAdmin = false;
+  private data: any = [];  //los datos que me da el backEnd
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
     const userData = { email, password };
-    this.isAuthenticated = true;
-    return this.http.post(`${baseURL}/api/user/login`, userData);
-    // Esto tiene que retornar esto ???
+    this.data = this.http.post(`${baseURL}/api/user/login`, userData);
+
+    // CONTROLAR ESTO
+    if ('con la nueva respuesta buscar si se loggeo OK o no y ahi recien cambiar a true') { }
+    if (true) {
+      this.isAuthenticated = true;
+    }
+
+    // CONTROLAR ESTO TAMBIEN
+    if ('con la nueva respuesta del backend buscar en este data algo sobre admin') {
+      this.isAdmin = true;
+    }
+
+    return this.data
   }
 
   logout(): void {
     this.isAuthenticated = false;
+    this.isAdmin = false;
   }
 
   isLoggedIn(): boolean {
     return this.isAuthenticated;
+  }
+
+  isAdministrator(): boolean {
+    return this.isAdmin;
   }
 
 }
