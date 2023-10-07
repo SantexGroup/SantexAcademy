b15beaee79fbfade57bbc753062e34223ca8f870
Sebastian Ledesma <sevaledesma@gmail.com>
2023-10-06 22:14:11 -0300

    FE - crear la pagina de noticias, con tarjetas hardcodeadas

--- front/src/app/modules/news/news-routing.module.ts
+++ front/src/app/modules/news/news-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './components/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NewsComponent,
+    children: [
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
