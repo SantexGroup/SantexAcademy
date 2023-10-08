1d87636994c524cf8be0ebfbc02d36234ead1325
koritacrah <juanmcoro2003@gmail.com>
2023-10-08 13:13:57 -0300

    login front

--- front/src/app/modules/dashboard/login/login.component.ts
+++ front/src/app/modules/dashboard/login/login.component.ts
@@ -8,7 +8,7 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  error: string =''
   myForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required])
@@ -19,7 +19,18 @@ export class LoginComponent implements OnInit {
   ingresarUs () {
     this.userServ.login(this.myForm.value).subscribe(
       (res) => {
-        console.log(res);
+        // if(res.token){ esto es si tenemos jwt
+        //   localStorage.setItem('token', res.token);
+        //   localStorage.setItem('user', JSON.stringify(res.user));
+        // }
+        if(res.user){
+          localStorage.setItem('user', JSON.stringify(res.user));
+        }else{
+          this.error = "error en la solicitud"
+        }
+      },
+      (error) => {
+        this.error = error.error.msg + ", intente nuevamente"; // Manejo de errores de solicitud
       }
     )
   }
