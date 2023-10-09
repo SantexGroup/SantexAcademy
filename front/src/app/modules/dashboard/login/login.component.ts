884bdbb579ed11753bcc4ba8718b5f006d84fc68
koritacrah <juanmcoro2003@gmail.com>
2023-10-09 19:06:28 -0300

    manejo de opciones y vista segun tipo de usuario

--- front/src/app/modules/dashboard/login/login.component.ts
+++ front/src/app/modules/dashboard/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/core/services/user.service';
-
+import { Router } from '@angular/router';
+import { User } from 'src/app/core/interfaces/user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,8 +14,8 @@ export class LoginComponent implements OnInit {
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required])
   });
-
-  constructor(private userServ: UserService) { }
+  actualUser : User | undefined;
+  constructor(private userServ: UserService, private router: Router) { }
   
   ingresarUs () {
     this.userServ.login(this.myForm.value).subscribe(
@@ -25,6 +26,7 @@ export class LoginComponent implements OnInit {
         // }
         if(res.user){
           localStorage.setItem('user', JSON.stringify(res.user));
+          this.router.navigate([this.userServ.getPreviousUrl() || ['/']]);
         }else{
           this.error = "error en la solicitud"
         }
--- front/src/app/modules/dashboard/nav-bar/nav-bar.component.ts
+++ front/src/app/modules/dashboard/nav-bar/nav-bar.component.ts
@@ -4,11 +4,13 @@ import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 
 export interface menuItem {
   name: string,
   paht: string,
+  type: number
 }
 
 @Component({
@@ -18,20 +20,26 @@ export interface menuItem {
 })
 export class NavBarComponent implements OnInit {
   
-  menu: menuItem[] = [{name: "Home", paht: "home"}, {name: "Agregar Producto", paht: "form-add-product"}, {name: "Actualizar Producto", paht:"form-update"}, {name: "Buscar Producto", paht: "search-product"}, {name: "Eliminar Producto", paht: "form-delete"}];
+  menu: menuItem[] = [{name: "Home", paht: "home", type:0}, {name: "Agregar Producto", paht: "form-add-product", type:1}, {name: "Actualizar Producto", paht:"form-update", type:1}, {name: "Buscar Producto", paht: "search-product", type:0}, {name: "Eliminar Producto", paht: "form-delete", type:1}];
   
   isMenuOpen: boolean = false;
   isDropdownOpen: boolean = false;
 
   isAuthenticated: boolean = false;
-  
+  userType:number=0
   total$: Observable<number>;
-
-  constructor(private shoppingCartService: ShoppingCartService, private elRef: ElementRef,private router: Router) { 
+  user: any = localStorage.getItem("user")
+  constructor(private shoppingCartService: ShoppingCartService, private elRef: ElementRef,private router: Router, private userServ: UserService) { 
     this.total$ = this.shoppingCartService.obtenerCarrito("1").pipe(
         map(products => products[0].Products.length)
       );
     this.isDropdownOpen  = false;
+    this.isAuthenticated = this.userServ.isLoggedIn();
+    if(this.user){
+      this.user = JSON.parse(this.user)
+      this.userType = this.user.type
+      console.log(this.userType)
+    }    
   }
 
   @HostListener('document:click', ['$event'])
@@ -63,13 +71,14 @@ export class NavBarComponent implements OnInit {
       //this.router.navigate(['./dashboard/home']);
     } else {
       // Si el usuario no está autenticado, redirige al usuario a la página de inicio de sesión.
+      this.userServ.setPreviousUrl('/')
       this.router.navigate(['./dashboard/login']);
     }
   }
 
   logout() {
     // Simula lógica de cierre de sesión
-    this.isAuthenticated = false;
+    this.userServ.logout();
     //this.router.navigate(['./dashboard/login']);
   }
 
