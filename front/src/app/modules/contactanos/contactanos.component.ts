341aaec79291cbed73fc992a1a8b5f5165375824
Nievas2 <104918140+Nievas2@users.noreply.github.com>
2023-10-09 16:53:00 -0300

    Update contactanos.component.ts

--- front/src/app/modules/contactanos/contactanos.component.ts
+++ front/src/app/modules/contactanos/contactanos.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactanos',
@@ -8,16 +7,5 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CONTACTANOSComponent {
 
-  // datos:FormGroup;
-  constructor(){
-  //   this.datos= new FormGroup({
-  //     usuario: new FormControl('',Validators.required),
-  //     email: new FormControl('', [Validators.required, Validators.email]),
-  //     phone: new FormGroup ('', Validators.required),
-  //     mensaje: new FormGroup ('', Validators.required)
-  //   })
-  // }
-  //  enviocorreo(){
-      
-     }
+  constructor(){}
 }
