7979debcabfe2464b50f0ecda9179f90207a93f6
koritacrah <juanmcoro2003@gmail.com>
2023-10-09 23:58:15 -0300

    conectando backend
    mostrando los datos de usuarios reales en la pagina d eusuario y mandnado los datos correctso al crear el usuario

--- front/src/app/modules/dashboard/register-users/register-users.component.ts
+++ front/src/app/modules/dashboard/register-users/register-users.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/core/interfaces/user';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-register-users',
   templateUrl: './register-users.component.html',
-  styleUrls: ['./register-users.component.css']
+  styleUrls: ['./register-users.component.css'],
 })
 export class RegisterUsersComponent implements OnInit {
-
   myForm = new FormGroup({
-    nombreUs: new FormControl('', [Validators.required]),
-    nombreCompleto: new FormControl('', [Validators.required]),
-    apellidoCompleto: new FormControl('', [Validators.required]),
-    emailUs: new FormControl('', [Validators.required]),
-    passwordUs: new FormControl('', [Validators.required])
+    username: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
   });
-  constructor() { }
+  newUser!: User;
+  registered: boolean = false;
+  constructor(private userServ: UserService, private router: Router) {
+    this.registered = false;
+  }
 
-  registrarUs () {
+  registrarUs() {
     // procedimieto de Ingreso
-    console.log('Registro a ABC Muebleria');
+    this.userServ.register(this.myForm.value).subscribe((res) => {
+      this.newUser = res.user;
+      if (this.newUser) {
+        console.log(this.newUser)
+        this.registered = true;
+      }
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  closePopup() {
+    // redirigir a anterior url
+    this.router.navigateByUrl(this.userServ.getPreviousUrl());
+  }
 }
