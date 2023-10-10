d78e02ec051e9c9cbfb49068256e2526bd0f3a7e
koritacrah <juanmcoro2003@gmail.com>
2023-10-09 23:57:07 -0300

    cambios minimos estilos login componnet

--- front/src/app/core/services/user.service.ts
+++ front/src/app/core/services/user.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../http/api.service';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private previousUrl: string = '/'; // Valor predeterminado como página de inicio
-
+  private userLogged!: User ;
   constructor(private _http: ApiService) { }
   login(data: any): Observable<any>{
     return this._http.post('http://localhost:4001/user/login',data);
+  }
+
+  register(data: any): Observable<any>{
+    return this._http.post('http://localhost:4001/user/',data);
   }
 
 
@@ -22,6 +27,7 @@ export class UserService {
      localStorage.removeItem('user');
   }
 
+ 
   setPreviousUrl(url: string): void {
     this.previousUrl = url;
   }
