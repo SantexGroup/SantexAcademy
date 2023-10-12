751a3252258994ec4049c8e24ac8ba0d2b566b04
ismael <ismaelreta.programacion@gmail.com>
2023-10-12 18:17:28 -0300

    constraint added

--- back/migrations/20230927155440-create-registered.js
+++ back/migrations/20230927155440-create-registered.js
@@ -7,7 +7,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       idCourse: {
         type: Sequelize.INTEGER,
@@ -25,15 +25,21 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       }
+    });
+    await queryInterface.addConstraint('Registereds', {
+      fields: ['idUser', 'idCourse'],
+      type: 'unique',
+      name: 'unique_user_course',
     });
   },
   async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('Registereds', 'unique_user_course');
     await queryInterface.dropTable('Registereds');
   }
 };
