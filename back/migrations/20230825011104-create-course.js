cbfefe05a0b564a6c2592f0640e780d1f3fc501a
CaroNew <125692041+CaroNew@users.noreply.github.com>
2023-10-15 17:21:01 -0300

    BE-validacion de campos cursos

--- back/migrations/20230825011104-create-course.js
+++ back/migrations/20230825011104-create-course.js
@@ -10,13 +10,16 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       }, 
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
