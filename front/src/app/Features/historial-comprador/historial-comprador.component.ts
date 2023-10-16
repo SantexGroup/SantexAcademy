fec789e67a0e4ecb0efec781ded98f70ecbd3c77
Leonardo Cortes <leosirx@gmail.com>
2023-10-16 13:27:45 -0300

    se agregaro autenticacion a las rutas FE

--- front/src/app/Features/historial-comprador/historial-comprador.component.ts
+++ front/src/app/Features/historial-comprador/historial-comprador.component.ts
@@ -34,11 +34,15 @@ export class HistorialCompradorComponent implements OnInit {
   datosLog: any = {};
   respuesta: any = [];
   servidor: string = environment.API_URL + '/images/';
+  
+  confUsuario: boolean = false;
+  idUser: string = '';
 
   constructor(private service: HisComService, private router: Router) { }
 
   ngOnInit(): void {
     this.datos();
+    this.getIdUser();
   }
 
   datos() {
@@ -112,5 +116,19 @@ export class HistorialCompradorComponent implements OnInit {
   redirigirProducto(idProd: number) {
     localStorage.setItem('idProd', idProd.toString());
     this.router.navigate(['vista-articulo']);
+  }
+  getIdUser() {    
+    let infoLocal = localStorage.getItem('resLog');
+    if (infoLocal) {
+      let newObject = JSON.parse(infoLocal);
+      const idUser = newObject[1].users.id;
+      this.idUser = idUser;
+      if (idUser) {
+        this.confUsuario = true; 
+      } else {
+        this.confUsuario = false; 
+      }
+      
+    }
   }
 }
