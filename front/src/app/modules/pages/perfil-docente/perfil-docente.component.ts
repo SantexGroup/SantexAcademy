dd89aa91843193f8cfd46656a9c6ab2a509c94dc
melisaapaz <melisaapaz@gmail.com>
2023-10-18 19:54:15 +0200

    Corrección de deslogueo desde perfil docente

--- front/src/app/modules/pages/perfil-docente/perfil-docente.component.ts
+++ front/src/app/modules/pages/perfil-docente/perfil-docente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-perfil-docente',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-docente.component.css']
 })
 export class PerfilDocenteComponent implements OnInit {
+  isLoggedIn: boolean = true;
+
   perfilDelDocente = {
     nombre: 'Nombre del Docente',
     edad: 20,
@@ -13,9 +17,19 @@ export class PerfilDocenteComponent implements OnInit {
 
 
     }
-  constructor() { }
+  constructor(private router: Router,private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {    
+    this.authService.logout();
+    console.log("Deslogueo")
+    this.router.navigateByUrl('/login');
+    
+    // Actualiza el estado de autenticación después del logout
+    this.authService.isAuthenticated$.subscribe((authenticated) => {
+      this.isLoggedIn = authenticated;
+    });
+  }
 }
